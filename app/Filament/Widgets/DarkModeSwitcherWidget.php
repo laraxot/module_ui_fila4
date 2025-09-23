01dbb49699cbb2d75c82b3337cafeff6059f34bf
marco76tv <marco.sottana@gmail.com>
2025-09-23 17:25:35 +0200

    .

--- app/Filament/Widgets/DarkModeSwitcherWidget.php
+++ app/Filament/Widgets/DarkModeSwitcherWidget.php
@@ -4,14 +4,9 @@ declare(strict_types=1);
 
 namespace Modules\UI\Filament\Widgets;
 
-use Filament\Forms\Components\Component;
+use Filament\Schemas\Components\Component;
 use Override;
-<<<<<<< HEAD
 use Filament\Schemas\Schema;
-=======
-use Filament\Forms\Form;
-use Filament\Forms\Form;
->>>>>>> e709487 (.)
 use Illuminate\Contracts\View\View;
 use Illuminate\Support\Facades\Cookie;
 use Modules\Xot\Filament\Widgets\XotBaseWidget;
