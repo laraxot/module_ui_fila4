553a89146dcd0f2fdcba5a1c687ce05eb6ebae1c
marco76tv <marco.sottana@gmail.com>
2025-09-23 13:09:33 +0200

    .

--- app/Filament/Tables/Columns/GroupColumn.php
+++ app/Filament/Tables/Columns/GroupColumn.php
@@ -5,6 +5,10 @@ declare(strict_types=1);
 namespace Modules\UI\Filament\Tables\Columns;
 
 use Filament\Tables\Columns\Column;
+<<<<<<< HEAD
+=======
+use Filament\Tables\Table;
+>>>>>>> 19f3650 (.)
 
 class GroupColumn extends Column
 {
@@ -23,4 +27,22 @@ class GroupColumn extends Column
 
         return $this;
     }
+<<<<<<< HEAD
+=======
+
+    /**
+     * Ensure child columns are mounted to the same table to avoid
+     * "The column [x] is not mounted to a table".
+     */
+    public function mount(Table $table): void
+    {
+        parent::mount($table);
+
+        foreach ($this->schema as $child) {
+            if ($child instanceof Column && null === $child->getTable()) {
+                $child->mount($table);
+            }
+        }
+    }
+>>>>>>> 19f3650 (.)
 }
