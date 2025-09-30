01a24740ec3068a0199850d37a692589c249368d
marco76tv <marco.sottana@gmail.com>
2025-09-30 21:23:55 +0200

    .

--- app/Filament/Forms/Components/RadioCollection.php
+++ app/Filament/Forms/Components/RadioCollection.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Modules\UI\Filament\Forms\Components;
+namespace Modules\Notify\Filament\Forms\Components;
 
 use Closure;
 use Filament\Forms\Components\Field;
