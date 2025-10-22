284fffe6038e0a9dab6ae4ed3dd66592491e1b5e
marco76tv <marco.sottana@gmail.com>
2025-10-22 09:24:01 +0200

    .

--- app/Actions/Block/GetAllBlocksAction.php
+++ app/Actions/Block/GetAllBlocksAction.php
@@ -9,12 +9,46 @@ use Illuminate\Support\Facades\File;
 use Illuminate\Support\Str;
 use Modules\Xot\Actions\File\GetClassNameByPathAction;
 use Modules\Xot\Datas\ComponentFileData;
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+
+use function Safe\realpath;
+
+>>>>>>> a12f125f4a (.)
+=======
+>>>>>>> b93ef594b4 (.)
+=======
+
+use function Safe\realpath;
+
+>>>>>>> origin/develop
+>>>>>>> 1899c5f (.)
 use Spatie\LaravelData\DataCollection;
 use Spatie\QueueableAction\QueueableAction;
 use Webmozart\Assert\Assert;
 
+<<<<<<< HEAD
 use function Safe\realpath;
 
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+use function Safe\realpath;
+
+=======
+>>>>>>> a12f125f4a (.)
+=======
+use function Safe\realpath;
+
+>>>>>>> b93ef594b4 (.)
+=======
+>>>>>>> origin/develop
+>>>>>>> 1899c5f (.)
 class GetAllBlocksAction
 {
     use QueueableAction;
@@ -22,6 +56,12 @@ class GetAllBlocksAction
     /**
      * @return DataCollection<ComponentFileData>
      */
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+>>>>>>> 1899c5f (.)
     public function execute(string $_context = 'form'): DataCollection
     {
         Assert::string($relativePath = config('modules.paths.generator.model.path'));
@@ -46,6 +86,73 @@ class GetAllBlocksAction
                 'path' => $path,
             ];
         });
+<<<<<<< HEAD
+=======
+=======
+    public function execute(string $context = 'form'): DataCollection
+=======
+    public function execute(string $_context = 'form'): DataCollection
+>>>>>>> b93ef594b4 (.)
+    {
+        Assert::string($relativePath = config('modules.paths.generator.model.path'));
+
+        $files = File::glob(base_path('Modules') . '/*/' . $relativePath . '/../Filament/Blocks/*.php');
+
+        $blocks = Arr::map($files, function (string $path) {
+            $path = realpath($path);
+            $class = app(GetClassNameByPathAction::class)->execute($path);
+
+            $name = Str::of(class_basename($class))->snake()->toString();
+            if (Str::endsWith($name, '_block')) {
+                $name = Str::before($name, '_block');
+            }
+<<<<<<< HEAD
+        );
+>>>>>>> a12f125f4a (.)
+=======
+
+            $module = Str::of($class)->between('Modules\\', '\Filament\\')->toString();
+
+            return [
+                'name' => $name,
+                'class' => $class,
+                'module' => $module,
+                'path' => $path,
+            ];
+        });
+>>>>>>> b93ef594b4 (.)
+=======
+    public function execute(string $context = 'form'): DataCollection
+    {
+        Assert::string($relativePath = config('modules.paths.generator.model.path'));
+
+        $files = File::glob(base_path('Modules').'/*/'.$relativePath.'/../Filament/Blocks/*.php');
+
+        $blocks = Arr::map(
+            $files,
+            function (string $path) {
+                $path = realpath($path);
+                $class = app(GetClassNameByPathAction::class)->execute($path);
+
+                $name = Str::of(class_basename($class))->snake()->toString();
+                if (Str::endsWith($name, '_block')) {
+                    $name = Str::before($name, '_block');
+                }
+
+                $module = Str::of($class)
+                    ->between('Modules\\', '\Filament\\')
+                    ->toString();
+
+                return [
+                    'name' => $name,
+                    'class' => $class,
+                    'module' => $module,
+                    'path' => $path,
+                ];
+            }
+        );
+>>>>>>> origin/develop
+>>>>>>> 1899c5f (.)
 
         return ComponentFileData::collection($blocks);
     }
--- resources/views/filament/widgets/stat-with-icon.blade.php
+++ resources/views/filament/widgets/stat-with-icon.blade.php
@@ -1,23 +1,79 @@
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> b93ef594b4 (.)
+>>>>>>> 1899c5f (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+>>>>>>> 1899c5f (.)
 <x-filament-widgets::widget>
     <x-filament::section collapsible>
         <div class="grid grid-cols-1 lg:grid-cols-2 gap-4">
             <div class="col-span-1">
+<<<<<<< HEAD
                 <span class="overflow-hidden" >
                 <x-filament::icon
                                 icon="fas-futbol"
+=======
+=======
+=======
+>>>>>>> origin/develop
+<x-filament-widgets::widget>
+    <x-filament::section collapsible>
+        <x-filament::grid lg='2'>
+            <x-filament::grid.column >
+<<<<<<< HEAD
+>>>>>>> a12f125f4a (.)
+=======
+<x-filament-widgets::widget>
+    <x-filament::section collapsible>
+        <div class="grid grid-cols-1 lg:grid-cols-2 gap-4">
+            <div class="col-span-1">
+>>>>>>> b93ef594b4 (.)
+=======
+>>>>>>> origin/develop
+                <span class="overflow-hidden" >
+                <x-filament::icon
+                                icon="fas-futbol"
+                                size="24"
+>>>>>>> 1899c5f (.)
                                 class="w-auto h-12"
                             />
                 </span>
 
                 BBB
 
+<<<<<<< HEAD
             </div>
         </div>
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+            </div>
+        </div>
+=======
+            </x-filament::grid.column>
+        </x-filament::grid>
+>>>>>>> a12f125f4a (.)
+=======
+            </div>
+        </div>
+>>>>>>> b93ef594b4 (.)
+=======
+            </x-filament::grid.column>
+        </x-filament::grid>
+>>>>>>> origin/develop
+>>>>>>> 1899c5f (.)
     </x-filament::section>
 </x-filament-widgets::widget>
 {{--
@@ -42,6 +98,10 @@ declare(strict_types=1);
 
                             <x-filament::icon
                                 icon="fas-futbol"
+<<<<<<< HEAD
+=======
+                                :size="24"
+>>>>>>> 1899c5f (.)
                                 class="w-auto h-24 absolute left-0 top-8 text-primary-500 opacity-20 dark:opacity-20 transition group-hover:scale-110 group-hover:-rotate-12 group-hover:opacity-40 dark:group-hover:opacity-80"
                             />
                             <span class="overlook-name">AAA</span>
@@ -55,3 +115,17 @@ declare(strict_types=1);
     </x-filament-widgets::widget>
 
 --}}
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+
+>>>>>>> a12f125f4a (.)
+=======
+>>>>>>> b93ef594b4 (.)
+=======
+
+>>>>>>> origin/develop
+>>>>>>> 1899c5f (.)
