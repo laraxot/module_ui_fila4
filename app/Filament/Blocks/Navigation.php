39788eebbb88226c521e848412f4ad84687fd117
marco76tv <marco.sottana@gmail.com>
2025-10-22 08:00:14 +0200

    .

--- app/Filament/Blocks/Navigation.php
+++ app/Filament/Blocks/Navigation.php
@@ -36,11 +36,11 @@ class Navigation extends XotBaseBlock
     public function getFormSchema(): array
     {
         return [
-            'items' => Repeater::make('items')
+            Repeater::make('items')
                 ->label(static::trans('blocks.navigation.fields.items.label'))
                 ->schema([
-                    'text' => TextInput::make('text')->label(static::trans('blocks.navigation.fields.text.label')),
-                    'url' => TextInput::make('url')->label(static::trans('blocks.navigation.fields.url.label')),
+                    TextInput::make('text')->label(static::trans('blocks.navigation.fields.text.label')),
+                    TextInput::make('url')->label(static::trans('blocks.navigation.fields.url.label')),
                 ]),
         ];
     }
