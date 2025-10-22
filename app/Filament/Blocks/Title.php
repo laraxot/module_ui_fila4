ed813bd7ab94cb117351509eead645bdb67adc70
marco76tv <marco.sottana@gmail.com>
2025-10-22 09:23:35 +0200

    .

--- app/Filament/Blocks/Title.php
+++ app/Filament/Blocks/Title.php
@@ -12,10 +12,6 @@ use Modules\Xot\Actions\View\GetViewsSiblingsAndSelfAction;
 use Modules\Xot\Filament\Blocks\XotBaseBlock;
 
 class Title // extends XotBaseBlock
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> ef3c5fa (.)
 {
     public static function make(string $name = 'title', string $context = 'form'): Block
     {
@@ -42,41 +38,4 @@ class Title // extends XotBaseBlock
             ])
             ->columns($context === 'form' ? 2 : 1);
     }
-<<<<<<< HEAD
-=======
-{public static function make(
-    string $name = 'title',
-    string $context = 'form',
-): Block {
-    // $view = 'ui::components.blocks.title.v1';
-    // $views = app(GetViewsSiblingsAndSelfAction::class)->execute($view);
-
-    $options = app(GetViewBlocksOptionsByTypeAction::class)
-        ->execute('title', false);
-
-    return Block::make($name)
-        ->schema(
-            [
-                TextInput::make('text')
-                    ->required(),
-
-                Select::make('level')
-                    ->options(
-                        [
-                            'h2' => 'h2',
-                            'h3' => 'h3',
-                            'h4' => 'h4',
-                        ]
-                    )
-                    ->afterStateHydrated(static fn ($state, $set) => $state || $set('level', 'h2')),
-
-                Select::make('view')
-                    ->options($options),
-            ]
-        )
-        ->columns('form' === $context ? 2 : 1);
-}
->>>>>>> 727968c (.)
-=======
->>>>>>> ef3c5fa (.)
 }
