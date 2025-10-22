7497ccac832a7b55d2e80817e5b718d6794851fe
marco76tv <marco.sottana@gmail.com>
2025-10-22 07:59:17 +0200

    .

--- resources/views/components/blocks/educational_materials.blade.php
+++ resources/views/components/blocks/educational_materials.blade.php
@@ -1,8 +1,11 @@
+<<<<<<< HEAD
 <?php
 
 declare(strict_types=1);
 
 ?>
+=======
+>>>>>>> 727968c (.)
 @props(['title' => 'Materiale Educativo'])
 
 <div class="bg-white p-6 rounded-lg shadow-md">
@@ -81,4 +84,8 @@ declare(strict_types=1);
             {{ $materials->links() }}
         </div>
     @endif
+<<<<<<< HEAD
 </div>
+=======
+</div> 
+>>>>>>> 727968c (.)
