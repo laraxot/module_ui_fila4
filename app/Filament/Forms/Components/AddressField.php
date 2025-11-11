cb66a486fc9256ad9c09922a962f874537d76fb9
marco76tv <marco.sottana@gmail.com>
2025-11-11 11:01:25 +0100

    .

--- app/Filament/Forms/Components/AddressField.php
+++ app/Filament/Forms/Components/AddressField.php
@@ -84,7 +84,7 @@ class AddressField extends Field
 
     public function getDefaultChildComponents(?string $key = null): array
     {
-        $result = [
+        return [
             Grid::make()->schema([
                 Select::make('country')->searchable(),
                 // ->getSearchResultsUsing(fn (string $query) => Country::where('name', 'like', "%{$query}%")->pluck('name', 'id'))
@@ -97,9 +97,6 @@ class AddressField extends Field
                 TextInput::make('zip')->maxLength(255),
             ]),
         ];
-        
-        /** @phpstan-ignore return.type */
-        return $result;
     }
 
     public function getRelationship(): string
