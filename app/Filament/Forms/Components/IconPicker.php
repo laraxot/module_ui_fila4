64043f95e6cc5d4dbe987d86c6a0dd0ac40dbe12
marco76tv <marco.sottana@gmail.com>
2025-11-12 15:30:09 +0100

    .

--- app/Filament/Forms/Components/IconPicker.php
+++ app/Filament/Forms/Components/IconPicker.php
@@ -37,7 +37,7 @@ class IconPicker extends TextInput
                         ->reactive()
                         ->live(),
                     RadioIcon::make('newstate')
-                        ->options(function (\Filament\Schemas\Components\Utilities\Get $get) use ($icons): array {
+                        ->options(function (Get $get) use ($icons): array {
                             $pack = $get('pack');
                             if (! is_string($pack)) {
                                 return [];
@@ -54,7 +54,7 @@ class IconPicker extends TextInput
                         ->inline()
                         ->inlineLabel(false),
                 ])
-                ->action(function (array $data, \Filament\Schemas\Components\Utilities\Set $set) {
+                ->action(function (array $data, Set $set) {
                     $set('icon', $data['newstate']);
                 }),
         );
