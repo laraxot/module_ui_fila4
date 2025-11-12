1a6473bad87898dfe938da85cd93597cd6c5897a
marco76tv <marco.sottana@gmail.com>
2025-11-12 15:35:17 +0100

    .

--- resources/views/ui/studio-selector.blade.php
+++ resources/views/ui/studio-selector.blade.php
@@ -1,11 +1,17 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> ef3c5fa (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
+<<<<<<< HEAD
 =======
 >>>>>>> 727968c (.)
+=======
+>>>>>>> ef3c5fa (.)
 {{-- Studio Selector Component - Card cliccabili per selezione studio --}}
 <div 
     x-data="{
@@ -167,7 +173,11 @@ declare(strict_types=1);
         </div>
     @endif
 <<<<<<< HEAD
+<<<<<<< HEAD
 </div>
 =======
 </div> 
 >>>>>>> 727968c (.)
+=======
+</div>
+>>>>>>> ef3c5fa (.)
