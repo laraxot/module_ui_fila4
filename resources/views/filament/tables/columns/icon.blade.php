9271a0848e5f941f0efa2229eec7401e3c6bbb9f
marco76tv <marco.sottana@gmail.com>
2025-11-12 11:19:54 +0100

    .

--- resources/views/filament/tables/columns/icon.blade.php
+++ resources/views/filament/tables/columns/icon.blade.php
@@ -1,44 +1,10 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> b93ef594b4 (.)
->>>>>>> 1899c5f (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
->>>>>>> a12f125f4a (.)
-=======
->>>>>>> b93ef594b4 (.)
-=======
->>>>>>> origin/develop
->>>>>>> 1899c5f (.)
 <div class="filament-icon-picker-icon-column px-4 py-3">
 	@if($icon = $getState())
 		<x-icon class="h-6" name="{{$icon}}" />
 	@endif
-<<<<<<< HEAD
 </div>
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-</div>
-=======
-</div>
->>>>>>> a12f125f4a (.)
-=======
-</div>
->>>>>>> b93ef594b4 (.)
-=======
-</div>
->>>>>>> origin/develop
->>>>>>> 1899c5f (.)
