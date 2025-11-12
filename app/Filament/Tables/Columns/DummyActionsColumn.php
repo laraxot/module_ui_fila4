54f1e6d19bca596fa781ea615b40fe886424cc29
marco76tv <marco.sottana@gmail.com>
2025-11-12 15:42:16 +0100

    .

--- app/Filament/Tables/Columns/DummyActionsColumn.php
+++ app/Filament/Tables/Columns/DummyActionsColumn.php
@@ -19,8 +19,12 @@ class DummyActionsColumn extends ColumnGroup
         return $this;
     }
 
+    /**
+     * @return array<string, mixed>
+     */
     public function getActions(): array
     {
+        /** @var array<string, mixed> */
         return $this->actions;
     }
 
@@ -38,7 +42,7 @@ class DummyActionsColumn extends ColumnGroup
                 ->extraCellAttributes(['class' => 'px-0'])
                 ->action(Action::make('uno-action')
                     ->label('Prova Action')
-                    ->action(function () {
+                    ->action(function (): void {
                         dddx('Azione prova eseguita1!');
                     })),
             IconColumn::make('due')
@@ -48,7 +52,7 @@ class DummyActionsColumn extends ColumnGroup
                 ->extraCellAttributes(['class' => 'px-0'])
                 ->action(Action::make('due-action')
                     ->label('Prova Action')
-                    ->action(function () {
+                    ->action(function (): void {
                         dddx('Azione prova eseguita2!');
                     })),
         ]);
