7087443964b55598f8589734cb745a69b21a4b79
marco76tv <marco.sottana@gmail.com>
2025-11-12 15:31:47 +0100

    .

--- app/Filament/Forms/Components/IconPicker.php
+++ app/Filament/Forms/Components/IconPicker.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Modules\Notify\Filament\Forms\Components;
+namespace Modules\UI\Filament\Forms\Components;
 
 use Filament\Actions\Action;
 use Filament\Forms\Components\Select;
