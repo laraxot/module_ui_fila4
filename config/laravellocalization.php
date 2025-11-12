58e6777cd32b1adb30371b13a8191116afbb0073
marco76tv <marco.sottana@gmail.com>
2025-11-12 15:38:21 +0100

    .

--- config/laravellocalization.php
+++ config/laravellocalization.php
@@ -1,5 +1,11 @@
 <?php
 
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+>>>>>>> 1899c5f (.)
 declare(strict_types=1);
 
 
@@ -12,17 +18,72 @@ return [
      * | This is the array for the languages
      * |
      */
+<<<<<<< HEAD
+=======
+=======
+=======
+>>>>>>> origin/develop
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Languages
+    |--------------------------------------------------------------------------
+    |
+    | This is the array for the languages
+    |
+    */
+<<<<<<< HEAD
+>>>>>>> a12f125f4a (.)
+=======
+declare(strict_types=1);
+
+
+return [
+    /*
+     * |--------------------------------------------------------------------------
+     * | Languages
+     * |--------------------------------------------------------------------------
+     * |
+     * | This is the array for the languages
+     * |
+     */
+>>>>>>> b93ef594b4 (.)
+=======
+>>>>>>> origin/develop
+>>>>>>> 1899c5f (.)
     'languages' => [
         'it' => [
             'name' => 'Italiano',
             'script' => 'Latn',
             'native' => 'Italiano',
+<<<<<<< HEAD
             'regional' => 'it_IT',
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+            'regional' => 'it_IT',
+=======
+            'regional' => 'it_IT'
+>>>>>>> a12f125f4a (.)
+=======
+            'regional' => 'it_IT',
+>>>>>>> b93ef594b4 (.)
+=======
+            'regional' => 'it_IT'
+>>>>>>> origin/develop
+>>>>>>> 1899c5f (.)
         ],
         'en' => [
             'name' => 'English',
             'script' => 'Latn',
             'native' => 'English',
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+>>>>>>> 1899c5f (.)
             'regional' => 'en_GB',
         ],
     ],
@@ -34,6 +95,46 @@ return [
      * | This is the configuration for the language detection
      * |
      */
+<<<<<<< HEAD
+=======
+=======
+            'regional' => 'en_GB'
+        ]
+=======
+            'regional' => 'en_GB',
+        ],
+>>>>>>> b93ef594b4 (.)
+    ],
+    /*
+<<<<<<< HEAD
+=======
+            'regional' => 'en_GB'
+        ]
+    ],
+
+    /*
+>>>>>>> origin/develop
+    |--------------------------------------------------------------------------
+    | Language Detection
+    |--------------------------------------------------------------------------
+    |
+    | This is the configuration for the language detection
+    |
+    */
+<<<<<<< HEAD
+>>>>>>> a12f125f4a (.)
+=======
+     * |--------------------------------------------------------------------------
+     * | Language Detection
+     * |--------------------------------------------------------------------------
+     * |
+     * | This is the configuration for the language detection
+     * |
+     */
+>>>>>>> b93ef594b4 (.)
+=======
+>>>>>>> origin/develop
+>>>>>>> 1899c5f (.)
     'detectBrowserLanguage' => true,
     'detectBrowserLanguageFromAcceptLanguage' => true,
     'detectBrowserLanguageFromAcceptLanguageHeader' => true,
@@ -41,6 +142,12 @@ return [
     'detectBrowserLanguageFromCookie' => true,
     'detectBrowserLanguageFromQueryString' => true,
     'detectBrowserLanguageFromRoute' => true,
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+>>>>>>> 1899c5f (.)
     /*
      * |--------------------------------------------------------------------------
      * | Language Selection
@@ -49,12 +156,47 @@ return [
      * | This is the configuration for the language selection
      * |
      */
+<<<<<<< HEAD
+=======
+=======
+=======
+>>>>>>> origin/develop
+
+    /*
+    |--------------------------------------------------------------------------
+    | Language Selection
+    |--------------------------------------------------------------------------
+    |
+    | This is the configuration for the language selection
+    |
+    */
+<<<<<<< HEAD
+>>>>>>> a12f125f4a (.)
+=======
+    /*
+     * |--------------------------------------------------------------------------
+     * | Language Selection
+     * |--------------------------------------------------------------------------
+     * |
+     * | This is the configuration for the language selection
+     * |
+     */
+>>>>>>> b93ef594b4 (.)
+=======
+>>>>>>> origin/develop
+>>>>>>> 1899c5f (.)
     'hideDefaultLocaleInURL' => false,
     'useAcceptLanguageHeader' => true,
     'useSessionLocale' => true,
     'useCookieLocale' => true,
     'useQueryStringLocale' => true,
     'useRouteLocale' => true,
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+>>>>>>> 1899c5f (.)
     /*
      * |--------------------------------------------------------------------------
      * | Language Redirect
@@ -63,6 +205,35 @@ return [
      * | This is the configuration for the language redirect
      * |
      */
+<<<<<<< HEAD
+=======
+=======
+=======
+>>>>>>> origin/develop
+
+    /*
+    |--------------------------------------------------------------------------
+    | Language Redirect
+    |--------------------------------------------------------------------------
+    |
+    | This is the configuration for the language redirect
+    |
+    */
+<<<<<<< HEAD
+>>>>>>> a12f125f4a (.)
+=======
+    /*
+     * |--------------------------------------------------------------------------
+     * | Language Redirect
+     * |--------------------------------------------------------------------------
+     * |
+     * | This is the configuration for the language redirect
+     * |
+     */
+>>>>>>> b93ef594b4 (.)
+=======
+>>>>>>> origin/develop
+>>>>>>> 1899c5f (.)
     'redirectToDefaultLocale' => true,
     'redirectToDefaultLocaleIfNotSupported' => true,
     'redirectToDefaultLocaleIfNotInSupportedLocales' => true,
@@ -72,6 +243,12 @@ return [
     'redirectToDefaultLocaleIfNotInSupportedLocalesAndNotInCookie' => true,
     'redirectToDefaultLocaleIfNotInSupportedLocalesAndNotInQueryString' => true,
     'redirectToDefaultLocaleIfNotInSupportedLocalesAndNotInRoute' => true,
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+>>>>>>> 1899c5f (.)
     /*
      * |--------------------------------------------------------------------------
      * | Language Routes
@@ -80,6 +257,35 @@ return [
      * | This is the configuration for the language routes
      * |
      */
+<<<<<<< HEAD
+=======
+=======
+=======
+>>>>>>> origin/develop
+
+    /*
+    |--------------------------------------------------------------------------
+    | Language Routes
+    |--------------------------------------------------------------------------
+    |
+    | This is the configuration for the language routes
+    |
+    */
+<<<<<<< HEAD
+>>>>>>> a12f125f4a (.)
+=======
+    /*
+     * |--------------------------------------------------------------------------
+     * | Language Routes
+     * |--------------------------------------------------------------------------
+     * |
+     * | This is the configuration for the language routes
+     * |
+     */
+>>>>>>> b93ef594b4 (.)
+=======
+>>>>>>> origin/develop
+>>>>>>> 1899c5f (.)
     'useLocalizedRoutes' => true,
     'useLocalizedRoutesInMiddleware' => true,
     'useLocalizedRoutesInController' => true,
--- lang/en/pages.php
+++ lang/en/pages.php
@@ -46,4 +46,20 @@ return [
             'email_sent_error' => 'Error sending email',
         ],
     ],
+<<<<<<< HEAD
 ];
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+];
+=======
+]; 
+>>>>>>> a12f125f4a (.)
+=======
+];
+>>>>>>> b93ef594b4 (.)
+=======
+]; 
+>>>>>>> origin/develop
+>>>>>>> 1899c5f (.)
