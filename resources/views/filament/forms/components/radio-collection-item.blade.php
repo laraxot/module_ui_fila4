5228d7a6cce6eafde46b9b49529679261849d34f
marco76tv <marco.sottana@gmail.com>
2025-11-12 11:21:53 +0100

    .

--- resources/views/filament/forms/components/radio-collection-item.blade.php
+++ resources/views/filament/forms/components/radio-collection-item.blade.php
@@ -1,8 +1,11 @@
+<<<<<<< HEAD
 <?php
 
 declare(strict_types=1);
 
 ?>
+=======
+>>>>>>> 727968c (.)
 {{-- resources/views/forms/components/radio-collection-item.blade.php --}}
 {{-- Template di esempio per un item - personalizzalo secondo le tue esigenze --}}
 <div class="flex items-center">
@@ -27,4 +30,8 @@ declare(strict_types=1);
             </div>
         @endif
     </div>
+<<<<<<< HEAD
 </div>
+=======
+</div>
+>>>>>>> 727968c (.)
