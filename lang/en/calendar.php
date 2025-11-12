287482ec241a4122c1a118b3782f2f285be9e8e0
marco76tv <marco.sottana@gmail.com>
2025-11-12 11:48:02 +0100

    .

--- lang/en/calendar.php
+++ lang/en/calendar.php
@@ -7,14 +7,6 @@ return [
     'next_month' => 'Next month',
     'today' => 'Today',
     'months' => [
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> b93ef594b4 (.)
->>>>>>> 1899c5f (.)
         'January',
         'February',
         'March',
@@ -27,10 +19,6 @@ return [
         'October',
         'November',
         'December',
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 1899c5f (.)
     ],
     'weekdays' => [
         'Sunday',
@@ -40,32 +28,6 @@ return [
         'Thursday',
         'Friday',
         'Saturday',
-<<<<<<< HEAD
-=======
-=======
-=======
->>>>>>> origin/develop
-        'January', 'February', 'March', 'April', 'May', 'June',
-        'July', 'August', 'September', 'October', 'November', 'December',
-    ],
-    'weekdays' => [
-        'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday',
-<<<<<<< HEAD
->>>>>>> a12f125f4a (.)
-=======
-    ],
-    'weekdays' => [
-        'Sunday',
-        'Monday',
-        'Tuesday',
-        'Wednesday',
-        'Thursday',
-        'Friday',
-        'Saturday',
->>>>>>> b93ef594b4 (.)
-=======
->>>>>>> origin/develop
->>>>>>> 1899c5f (.)
     ],
     'weekdays_short' => ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
 ];
--- resources/views/components/blocks/hero/v1.blade.php
+++ resources/views/components/blocks/hero/v1.blade.php
@@ -1,26 +1,8 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> b93ef594b4 (.)
->>>>>>> 1899c5f (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
->>>>>>> a12f125f4a (.)
-=======
->>>>>>> b93ef594b4 (.)
-=======
->>>>>>> origin/develop
->>>>>>> 1899c5f (.)
 @props(['title', 'subtitle', 'image', 'cta_text', 'cta_link'])
 
 <section class="relative bg-white overflow-hidden">
@@ -50,20 +32,4 @@ declare(strict_types=1);
             <img class="absolute inset-0 w-full h-full object-cover" src="{{ $image }}" alt="Hero background">
         </div>
     </div>
-<<<<<<< HEAD
 </section>
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-</section>
-=======
-</section> 
->>>>>>> a12f125f4a (.)
-=======
-</section>
->>>>>>> b93ef594b4 (.)
-=======
-</section> 
->>>>>>> origin/develop
->>>>>>> 1899c5f (.)
