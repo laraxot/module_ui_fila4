209699d584d9d61683ea02da2602e7dfb37a2352
marco76tv <marco.sottana@gmail.com>
2025-11-12 15:39:39 +0100

    .

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,26 +1,8 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> b93ef594b4 (.)
->>>>>>> 1899c5f (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
->>>>>>> a12f125f4a (.)
-=======
->>>>>>> b93ef594b4 (.)
-=======
->>>>>>> origin/develop
->>>>>>> 1899c5f (.)
 <x-layouts.main>
     
     <x-ui.app.header />
@@ -40,20 +22,4 @@ declare(strict_types=1);
         </div>
     </div>
 
-<<<<<<< HEAD
 </x-layouts.main>
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-</x-layouts.main>
-=======
-</x-layouts.main>
->>>>>>> a12f125f4a (.)
-=======
-</x-layouts.main>
->>>>>>> b93ef594b4 (.)
-=======
-</x-layouts.main>
->>>>>>> origin/develop
->>>>>>> 1899c5f (.)
