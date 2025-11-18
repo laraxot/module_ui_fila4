556c5796edc97eb3a5510f973fac3d397e95ec27
marco76tv <marco.sottana@gmail.com>
2025-11-18 10:10:07 +0100

    .

--- app/Filament/Forms/Components/RadioBadge.php
+++ app/Filament/Forms/Components/RadioBadge.php
@@ -57,7 +57,7 @@ class RadioBadge extends Radio
         return $color ?? $this->selectedColor;
     }
 
-    public function getIconForOption(string $value): \Illuminate\Contracts\Support\Htmlable|string|null
+    public function getIconForOption(string $value): string|null
     {
         $icon = $this->getEnumValue($value)?->getIcon();
 
