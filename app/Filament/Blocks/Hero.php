2e3115759d5475a4dd4a299030795a3af19e085b
marco76tv <marco.sottana@gmail.com>
2025-12-09 18:42:58 +0100

    .

--- app/Filament/Blocks/Hero.php
+++ app/Filament/Blocks/Hero.php
@@ -10,11 +10,7 @@ use Filament\Forms\Components\Repeater;
 use Filament\Forms\Components\RichEditor;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
-<<<<<<< HEAD
 use \Filament\Forms\Forms\Components\RadioImage;
-=======
-use Modules\UI\Filament\Forms\Components\RadioImage;
->>>>>>> e3274ea (.)
 use Modules\Xot\Actions\Filament\Block\GetViewBlocksOptionsByTypeAction;
 
 class Hero
