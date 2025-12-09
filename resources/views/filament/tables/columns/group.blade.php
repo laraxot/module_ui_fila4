e2106d536fc626e04fc03e67b041b4d544aaa05e
marco76tv <marco.sottana@gmail.com>
2025-12-09 19:09:59 +0100

    .

--- resources/views/filament/tables/columns/group.blade.php
+++ resources/views/filament/tables/columns/group.blade.php
@@ -2,11 +2,16 @@
 
 declare(strict_types=1);
 
+    $fields = $getFields();
+    $record = $getRecord();
 ?>
+<<<<<<< HEAD
 @php
     $fields = $getFields();
     $record = $getRecord();
 @endphp
+=======
+>>>>>>> c59340d4 (.)
 <div
     {{
         $attributes
@@ -24,18 +29,50 @@ declare(strict_types=1);
         @php
             $name = $field->getName();
             $value = $record->{$name} ?? null;
+<<<<<<< HEAD
             
+=======
+
+>>>>>>> c59340d4 (.)
             // Skip empty values to save space
             if (empty($value) && $value !== 0 && $value !== '0') {
                 continue;
             }
-            
+
             // Format the value for display
             $formattedValue = $value;
+<<<<<<< HEAD
             
             // Use translation for label following Laraxot pattern
             $label = __('ui::table.columns.' . $name . '.label');
             $displayText = $label . ': ' . $formattedValue;
+=======
+
+            // Resolve the label leveraging LangServiceProvider auto translations
+            $rawLabel = $field->getLabel();
+
+            if ($rawLabel instanceof \Closure) {
+                $rawLabel = $rawLabel($record);
+            }
+
+            if ($rawLabel instanceof \Illuminate\Contracts\Support\Htmlable) {
+                $labelText = trim(strip_tags($rawLabel->toHtml()));
+            } elseif (is_string($rawLabel)) {
+                $labelText = trim($rawLabel);
+            } else {
+                $labelText = '';
+            }
+
+            if ($labelText === '') {
+                $translationKey = 'ui::table.columns.' . $name . '.label';
+                $translated = __($translationKey);
+                $labelText = $translated !== $translationKey
+                    ? $translated
+                    : \Illuminate\Support\Str::of((string) $name)->replace('_', ' ')->headline()->value();
+            }
+
+            $displayText = $labelText . ': ' . $formattedValue;
+>>>>>>> c59340d4 (.)
         @endphp
         
             {{ $displayText }}<br/>
