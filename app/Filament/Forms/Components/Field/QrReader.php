f260abe7c82977a16a417647cc16f43eedea25bf
marco76tv <marco.sottana@gmail.com>
2025-12-09 19:05:25 +0100

    .

--- app/Filament/Forms/Components/Field/QrReader.php
+++ app/Filament/Forms/Components/Field/QrReader.php
@@ -11,10 +11,6 @@ class QrReader extends Field
     protected string $view = 'ui::filament.forms.components.field.qr-reader';
 
     /*
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> ef3c5fa (.)
      * public static function make($livewire): static
      * {
      * $result = app(static::class, ['livewire' => $livewire]);
@@ -22,16 +18,4 @@ class QrReader extends Field
      * return $result;
      * }
      */
-<<<<<<< HEAD
-=======
-    public static function make($livewire): static
-    {
-        $result = app(static::class, ['livewire' => $livewire]);
-        $result->configure();
-        return $result;
-    }
-    */
->>>>>>> 727968c (.)
-=======
->>>>>>> ef3c5fa (.)
 }
