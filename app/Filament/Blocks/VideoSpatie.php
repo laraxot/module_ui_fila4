db1cce1b8320d1106c8afc43c7e93350914629ea
marco76tv <marco.sottana@gmail.com>
2025-12-09 18:52:32 +0100

    .

--- app/Filament/Blocks/VideoSpatie.php
+++ app/Filament/Blocks/VideoSpatie.php
@@ -70,8 +70,13 @@ class VideoSpatie
                 TextInput::make('caption'),
                 // ->columnSpanFull()
                 // Filament\Forms\Components\SpatieMediaLibraryFileUpload::whereCustomProperties does not exist.
+<<<<<<< HEAD
                 // ->whereCustomProperties(fn(Forms\\Filament\Schemas\Components\Utilities\Get $get) => ['gallery_id' => $get('gallery_id')])
                 // ->customProperties(fn(Forms\\Filament\Schemas\Components\Utilities\Get $get) => ['gallery_id' => $get('gallery_id')]),
+=======
+                // ->whereCustomProperties(fn(Forms\Get $get) => ['gallery_id' => $get('gallery_id')])
+                // ->customProperties(fn(Forms\Get $get) => ['gallery_id' => $get('gallery_id')]),
+>>>>>>> e3274ea (.)
                 // Forms\Components\SpatieMediaLibraryFileUpload::make('media_id')
             ])
             ->columns($context === 'form' ? 2 : 1);
