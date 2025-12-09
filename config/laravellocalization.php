f5593a17a0a82a0de77524ff5fd8e3862c24a507
marco76tv <marco.sottana@gmail.com>
2025-12-09 19:07:01 +0100

    .

--- config/laravellocalization.php
+++ config/laravellocalization.php
@@ -2,10 +2,7 @@
 
 declare(strict_types=1);
 
-<<<<<<< HEAD
 
-=======
->>>>>>> 930be57 (.)
 return [
     /*
      * |--------------------------------------------------------------------------
@@ -99,8 +96,4 @@ return [
     'useLocalizedRoutesInRouteModelBindingWithSlugAndLocaleAndFallbackAndRedirectAndSessionAndCookie' => true,
     'useLocalizedRoutesInRouteModelBindingWithSlugAndLocaleAndFallbackAndRedirectAndSessionAndCookieAndQueryString' => true,
     'useLocalizedRoutesInRouteModelBindingWithSlugAndLocaleAndFallbackAndRedirectAndSessionAndCookieAndQueryStringAndRoute' => true,
-<<<<<<< HEAD
 ];
-=======
-];
->>>>>>> 930be57 (.)
