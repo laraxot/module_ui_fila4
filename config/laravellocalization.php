24e55aed1111fe3446ee37026730e743e236a034
marco76tv <marco.sottana@gmail.com>
2025-12-09 19:01:28 +0100

    .

--- config/laravellocalization.php
+++ config/laravellocalization.php
@@ -1,11 +1,6 @@
 <?php
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> ef3c5fa (.)
 declare(strict_types=1);
-
 
 return [
     /*
@@ -16,43 +11,17 @@ return [
      * | This is the array for the languages
      * |
      */
-<<<<<<< HEAD
-=======
-return [
-    /*
-    |--------------------------------------------------------------------------
-    | Languages
-    |--------------------------------------------------------------------------
-    |
-    | This is the array for the languages
-    |
-    */
->>>>>>> 727968c (.)
-=======
->>>>>>> ef3c5fa (.)
     'languages' => [
         'it' => [
             'name' => 'Italiano',
             'script' => 'Latn',
             'native' => 'Italiano',
-<<<<<<< HEAD
-<<<<<<< HEAD
             'regional' => 'it_IT',
-=======
-            'regional' => 'it_IT'
->>>>>>> 727968c (.)
-=======
-            'regional' => 'it_IT',
->>>>>>> ef3c5fa (.)
         ],
         'en' => [
             'name' => 'English',
             'script' => 'Latn',
             'native' => 'English',
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> ef3c5fa (.)
             'regional' => 'en_GB',
         ],
     ],
@@ -64,23 +33,6 @@ return [
      * | This is the configuration for the language detection
      * |
      */
-<<<<<<< HEAD
-=======
-            'regional' => 'en_GB'
-        ]
-    ],
-
-    /*
-    |--------------------------------------------------------------------------
-    | Language Detection
-    |--------------------------------------------------------------------------
-    |
-    | This is the configuration for the language detection
-    |
-    */
->>>>>>> 727968c (.)
-=======
->>>>>>> ef3c5fa (.)
     'detectBrowserLanguage' => true,
     'detectBrowserLanguageFromAcceptLanguage' => true,
     'detectBrowserLanguageFromAcceptLanguageHeader' => true,
@@ -88,10 +40,6 @@ return [
     'detectBrowserLanguageFromCookie' => true,
     'detectBrowserLanguageFromQueryString' => true,
     'detectBrowserLanguageFromRoute' => true,
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> ef3c5fa (.)
     /*
      * |--------------------------------------------------------------------------
      * | Language Selection
@@ -100,30 +48,12 @@ return [
      * | This is the configuration for the language selection
      * |
      */
-<<<<<<< HEAD
-=======
-
-    /*
-    |--------------------------------------------------------------------------
-    | Language Selection
-    |--------------------------------------------------------------------------
-    |
-    | This is the configuration for the language selection
-    |
-    */
->>>>>>> 727968c (.)
-=======
->>>>>>> ef3c5fa (.)
     'hideDefaultLocaleInURL' => false,
     'useAcceptLanguageHeader' => true,
     'useSessionLocale' => true,
     'useCookieLocale' => true,
     'useQueryStringLocale' => true,
     'useRouteLocale' => true,
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> ef3c5fa (.)
     /*
      * |--------------------------------------------------------------------------
      * | Language Redirect
@@ -132,20 +62,6 @@ return [
      * | This is the configuration for the language redirect
      * |
      */
-<<<<<<< HEAD
-=======
-
-    /*
-    |--------------------------------------------------------------------------
-    | Language Redirect
-    |--------------------------------------------------------------------------
-    |
-    | This is the configuration for the language redirect
-    |
-    */
->>>>>>> 727968c (.)
-=======
->>>>>>> ef3c5fa (.)
     'redirectToDefaultLocale' => true,
     'redirectToDefaultLocaleIfNotSupported' => true,
     'redirectToDefaultLocaleIfNotInSupportedLocales' => true,
@@ -155,10 +71,6 @@ return [
     'redirectToDefaultLocaleIfNotInSupportedLocalesAndNotInCookie' => true,
     'redirectToDefaultLocaleIfNotInSupportedLocalesAndNotInQueryString' => true,
     'redirectToDefaultLocaleIfNotInSupportedLocalesAndNotInRoute' => true,
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> ef3c5fa (.)
     /*
      * |--------------------------------------------------------------------------
      * | Language Routes
@@ -167,20 +79,6 @@ return [
      * | This is the configuration for the language routes
      * |
      */
-<<<<<<< HEAD
-=======
-
-    /*
-    |--------------------------------------------------------------------------
-    | Language Routes
-    |--------------------------------------------------------------------------
-    |
-    | This is the configuration for the language routes
-    |
-    */
->>>>>>> 727968c (.)
-=======
->>>>>>> ef3c5fa (.)
     'useLocalizedRoutes' => true,
     'useLocalizedRoutesInMiddleware' => true,
     'useLocalizedRoutesInController' => true,
