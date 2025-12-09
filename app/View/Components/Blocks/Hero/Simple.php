d03cedbaf7cbdc90156bc08a9752b49a6ccc96ec
marco76tv <marco.sottana@gmail.com>
2025-12-09 18:42:37 +0100

    .

--- app/View/Components/Blocks/Hero/Simple.php
+++ app/View/Components/Blocks/Hero/Simple.php
@@ -1,8 +1,26 @@
 <?php
 
+<<<<<<< HEAD
 declare(strict_types=1);
 
 
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+declare(strict_types=1);
+
+
+=======
+>>>>>>> a12f125f4a (.)
+=======
+declare(strict_types=1);
+
+
+>>>>>>> b93ef594b4 (.)
+=======
+>>>>>>> origin/develop
+>>>>>>> 1899c5f (.)
 namespace Modules\UI\View\Components\Blocks\Hero;
 
 use Illuminate\View\Component;
@@ -11,11 +29,43 @@ class Simple extends Component
 {
     public function __construct()
     {
+<<<<<<< HEAD
         
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+        
+=======
+        //
+>>>>>>> a12f125f4a (.)
+=======
+        
+>>>>>>> b93ef594b4 (.)
+=======
+        //
+>>>>>>> origin/develop
+>>>>>>> 1899c5f (.)
     }
 
     public function render()
     {
         return view('ui::components.blocks.hero.simple');
     }
+<<<<<<< HEAD
 }
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+}
+=======
+} 
+>>>>>>> a12f125f4a (.)
+=======
+}
+>>>>>>> b93ef594b4 (.)
+=======
+} 
+>>>>>>> origin/develop
+>>>>>>> 1899c5f (.)
