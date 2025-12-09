3f25a8abb2c1e190a8c546f1b4b869e843e95d3e
marco76tv <marco.sottana@gmail.com>
2025-12-09 18:51:42 +0100

    .

--- app/Filament/Widgets/StatWithIconWidget.php
+++ app/Filament/Widgets/StatWithIconWidget.php
@@ -4,41 +4,18 @@ declare(strict_types=1);
 
 namespace Modules\UI\Filament\Widgets;
 
-<<<<<<< HEAD
 use Closure;
-=======
-<<<<<<< HEAD
-use Closure;
-=======
->>>>>>> origin/develop
->>>>>>> 1899c5f (.)
 use Filament\Widgets\Widget as BaseWidget;
 use Illuminate\Contracts\Support\Htmlable;
 
 class StatWithIconWidget extends BaseWidget
 {
-<<<<<<< HEAD
     protected string $view = 'ui::filament.widgets.stat-with-icon';
-=======
-<<<<<<< HEAD
-    protected string $view = 'ui::filament.widgets.stat-with-icon';
-=======
-    protected static string $view = 'ui::filament.widgets.stat-with-icon';
->>>>>>> origin/develop
->>>>>>> 1899c5f (.)
 
     protected string|Htmlable $label;
 
     /**
-<<<<<<< HEAD
      * @var scalar|Htmlable|Closure
-=======
-<<<<<<< HEAD
-     * @var scalar|Htmlable|Closure
-=======
-     * @var scalar|Htmlable|\Closure
->>>>>>> origin/develop
->>>>>>> 1899c5f (.)
      */
     protected $value;
 
