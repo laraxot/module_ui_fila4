9fbbbdc18b59d54b162a99350a35391e50916b80
marco76tv <marco.sottana@gmail.com>
2025-12-09 19:04:23 +0100

    .

--- app/Filament/Blocks/Heading.php
+++ app/Filament/Blocks/Heading.php
@@ -10,12 +10,6 @@ use Filament\Forms\Components\TextInput;
 
 class Heading
 {
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> 1899c5f (.)
     public static function make(string $name = 'heading', string $_context = 'form'): Block
     {
         return Block::make($name)
@@ -33,56 +27,5 @@ class Heading
                     ->required(),
             ])
             ->columns(2);
-<<<<<<< HEAD
-=======
-=======
-=======
->>>>>>> origin/develop
-    public static function make(
-        string $name = 'heading',
-        string $context = 'form',
-    ): Block {
-        return Block::make($name)
-            ->schema(
-                [
-                    TextInput::make('content')
-
-                        ->required(),
-                    Select::make('level')
-                        ->options([
-                            'h1' => 'Heading 1',
-                            'h2' => 'Heading 2',
-                            'h3' => 'Heading 3',
-                            'h4' => 'Heading 4',
-                            'h5' => 'Heading 5',
-                            'h6' => 'Heading 6',
-                        ])
-                        ->required(),
-                ]
-            )->columns(2);
-<<<<<<< HEAD
->>>>>>> a12f125f4a (.)
-=======
-    public static function make(string $name = 'heading', string $_context = 'form'): Block
-    {
-        return Block::make($name)
-            ->schema([
-                TextInput::make('content')->required(),
-                Select::make('level')
-                    ->options([
-                        'h1' => 'Heading 1',
-                        'h2' => 'Heading 2',
-                        'h3' => 'Heading 3',
-                        'h4' => 'Heading 4',
-                        'h5' => 'Heading 5',
-                        'h6' => 'Heading 6',
-                    ])
-                    ->required(),
-            ])
-            ->columns(2);
->>>>>>> b93ef594b4 (.)
-=======
->>>>>>> origin/develop
->>>>>>> 1899c5f (.)
     }
 }
