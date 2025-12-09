b950fcbb61cbb40a2ff1dae91682667b01a91cc3
marco76tv <marco.sottana@gmail.com>
2025-12-09 18:56:43 +0100

    .

--- app/Filament/Blocks/Contact.php
+++ app/Filament/Blocks/Contact.php
@@ -1,25 +1,11 @@
 <?php
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> ef3c5fa (.)
 declare(strict_types=1);
 
 namespace Modules\UI\Filament\Blocks;
 
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
-<<<<<<< HEAD
-=======
-namespace Modules\UI\Filament\Blocks;
-
-use Filament\Forms\Components\Textarea;
-use Filament\Forms\Components\TextInput;
-use Filament\Forms\Components\DateTimePicker;
->>>>>>> 727968c (.)
-=======
->>>>>>> ef3c5fa (.)
 use Modules\Xot\Filament\Blocks\XotBaseBlock;
 
 final class Contact extends XotBaseBlock
@@ -31,36 +17,15 @@ final class Contact extends XotBaseBlock
                 ->required()
                 ->label(__('ui::blocks.contact.fields.name.label'))
                 ->helperText(__('ui::blocks.contact.fields.name.helper_text')),
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
->>>>>>> 727968c (.)
-=======
->>>>>>> ef3c5fa (.)
             TextInput::make('email')
                 ->email()
                 ->required()
                 ->label(__('ui::blocks.contact.fields.email.label'))
                 ->helperText(__('ui::blocks.contact.fields.email.helper_text')),
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
->>>>>>> 727968c (.)
-=======
->>>>>>> ef3c5fa (.)
             TextInput::make('phone')
                 ->tel()
                 ->label(__('ui::blocks.contact.fields.phone.label'))
                 ->helperText(__('ui::blocks.contact.fields.phone.helper_text')),
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
->>>>>>> 727968c (.)
-=======
->>>>>>> ef3c5fa (.)
             Textarea::make('message')
                 ->required()
                 ->label(__('ui::blocks.contact.fields.message.label'))
@@ -72,12 +37,4 @@ final class Contact extends XotBaseBlock
     {
         return __('ui::blocks.contact.title');
     }
-<<<<<<< HEAD
-<<<<<<< HEAD
 }
-=======
-} 
->>>>>>> 727968c (.)
-=======
-}
->>>>>>> ef3c5fa (.)
