e36f9c192aad7788b45a664b529044e5209c89c6
marco76tv <marco.sottana@gmail.com>
2025-12-09 18:48:00 +0100

    .

--- lang/it/paragraph.php
+++ lang/it/paragraph.php
@@ -1,9 +1,5 @@
 <?php
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> ef3c5fa (.)
 declare(strict_types=1);
 
 return [
@@ -31,38 +27,3 @@ return [
         ],
     ],
 ];
-<<<<<<< HEAD
-=======
-return array (
-  'fields' => 
-  array (
-    'title' => 
-    array (
-      'label' => 'Titolo',
-      'description' => 'title',
-      'helper_text' => 'title',
-      'placeholder' => 'title',
-    ),
-    'text' => 
-    array (
-      'label' => 'Testo',
-      'description' => 'text',
-      'helper_text' => 'text',
-      'placeholder' => 'text',
-    ),
-    '_tpl' => 
-    array (
-      'label' => '_tpl',
-    ),
-    'view' => 
-    array (
-      'label' => 'view',
-      'description' => 'view',
-      'helper_text' => 'view',
-      'placeholder' => 'view',
-    ),
-  ),
-);
->>>>>>> 727968c (.)
-=======
->>>>>>> ef3c5fa (.)
