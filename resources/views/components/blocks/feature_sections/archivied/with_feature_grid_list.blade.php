9311b08754dec4bd95bd264328fd65a2a7eb8bdb
marco76tv <marco.sottana@gmail.com>
2025-12-09 19:06:39 +0100

    .

--- resources/views/components/blocks/feature_sections/archivied/with_feature_grid_list.blade.php
+++ resources/views/components/blocks/feature_sections/archivied/with_feature_grid_list.blade.php
@@ -1,11 +1,17 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> ef3c5fa (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
+<<<<<<< HEAD
 =======
 >>>>>>> 727968c (.)
+=======
+>>>>>>> ef3c5fa (.)
 <div class="bg-white">
     <div class="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:px-8 lg:py-40">
       <div class="mx-auto max-w-3xl text-center">
@@ -89,6 +95,9 @@ declare(strict_types=1);
     </div>
   </div>
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
   
 >>>>>>> 727968c (.)
+=======
+>>>>>>> ef3c5fa (.)
