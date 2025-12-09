93f1e5a5682f317bb8e85c321d1735a11b33805e
marco76tv <marco.sottana@gmail.com>
2025-12-09 19:04:01 +0100

    .

--- app/Filament/Blocks/Paragraph.php
+++ app/Filament/Blocks/Paragraph.php
@@ -14,6 +14,12 @@ use Modules\Xot\Actions\Filament\Block\GetViewBlocksOptionsByTypeAction;
 
 class Paragraph
 {
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+>>>>>>> 1899c5f (.)
     public static function make(string $name = 'paragraph', string $_context = 'form'): Block
     {
         // $view = 'ui::components.blocks.paragraph.v1';
@@ -28,5 +34,58 @@ class Paragraph
             // RadioImage::make('view')
             //    ->options($options),
         ]);
+<<<<<<< HEAD
+=======
+=======
+=======
+>>>>>>> origin/develop
+    public static function make(
+        string $name = 'paragraph',
+        string $context = 'form',
+    ): Block {
+<<<<<<< HEAD
+=======
+    public static function make(string $name = 'paragraph', string $_context = 'form'): Block
+    {
+>>>>>>> b93ef594b4 (.)
+        // $view = 'ui::components.blocks.paragraph.v1';
+        // $views = app(GetViewsSiblingsAndSelfAction::class)->execute($view);
+
+        $options = app(GetViewBlocksOptionsByTypeAction::class)->execute('paragraph', false);
+
+<<<<<<< HEAD
+=======
+        // $view = 'ui::components.blocks.paragraph.v1';
+        // $views = app(GetViewsSiblingsAndSelfAction::class)->execute($view);
+
+        $options = app(GetViewBlocksOptionsByTypeAction::class)
+            ->execute('paragraph', false);
+
+>>>>>>> origin/develop
+        return Block::make($name)
+            ->schema(
+                [
+                    TextInput::make('title'),
+                    RichEditor::make('text'),
+                    Select::make('view')
+                        ->options($options),
+                    // RadioImage::make('view')
+                    //    ->options($options),
+                ]
+            );
+<<<<<<< HEAD
+>>>>>>> a12f125f4a (.)
+=======
+        return Block::make($name)->schema([
+            TextInput::make('title'),
+            RichEditor::make('text'),
+            Select::make('view')->options($options),
+            // RadioImage::make('view')
+            //    ->options($options),
+        ]);
+>>>>>>> b93ef594b4 (.)
+=======
+>>>>>>> origin/develop
+>>>>>>> 1899c5f (.)
     }
 }
--- resources/views/components/menu/v1.blade.php
+++ resources/views/components/menu/v1.blade.php
@@ -1,8 +1,26 @@
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> b93ef594b4 (.)
+>>>>>>> 1899c5f (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+=======
+>>>>>>> a12f125f4a (.)
+=======
+>>>>>>> b93ef594b4 (.)
+=======
+>>>>>>> origin/develop
+>>>>>>> 1899c5f (.)
 <ul class="ml-auto flex items-center space-x-4">
         @foreach ($menu->items as $item)
             <li>
