85d33719f8735552bd31f6b3e54665f651016d01
marco76tv <marco.sottana@gmail.com>
2025-12-09 19:00:50 +0100

    .

--- resources/views/filament/clusters/test/pages/s3test.blade.php
+++ resources/views/filament/clusters/test/pages/s3test.blade.php
@@ -1,26 +1,8 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> b93ef594b4 (.)
->>>>>>> 1899c5f (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
->>>>>>> a12f125f4a (.)
-=======
->>>>>>> b93ef594b4 (.)
-=======
->>>>>>> origin/develop
->>>>>>> 1899c5f (.)
 {{--
     S3Test Page - Test page for email functionality
     View: ui::filament.clusters.test.pages.s3test
--- tests/Unit/Filament/Widgets/RowWidgetTest.php
+++ tests/Unit/Filament/Widgets/RowWidgetTest.php
@@ -2,43 +2,12 @@
 
 declare(strict_types=1);
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> 1899c5f (.)
 namespace Modules\UI\Tests\Unit\Filament\Widgets;
 
 use Filament\Widgets\Widget;
 use Illuminate\Contracts\View\View;
 use Modules\UI\Filament\Widgets\RowWidget;
 use Tests\TestCase;
-<<<<<<< HEAD
-=======
-=======
-namespace Modules\UI\Tests\Unit\Filament\Widgets\RowWidgetTest;
-
-=======
->>>>>>> b93ef594b4 (.)
-namespace Modules\UI\Tests\Unit\Filament\Widgets;
-
-use Illuminate\Contracts\View\View;
-use Filament\Widgets\Widget;
-use Modules\UI\Filament\Widgets\RowWidget;
-<<<<<<< HEAD
->>>>>>> a12f125f4a (.)
-=======
-use Tests\TestCase;
->>>>>>> b93ef594b4 (.)
-=======
-namespace Modules\UI\Tests\Unit\Filament\Widgets;
-
-use Modules\UI\Filament\Widgets\RowWidget;
-use Tests\TestCase;
-use Filament\Widgets\Widget;
->>>>>>> origin/develop
->>>>>>> 1899c5f (.)
 
 uses(TestCase::class);
 
@@ -71,25 +40,6 @@ test('row widget has proper properties', function (): void {
 test('row widget can render', function (): void {
     /** @phpstan-ignore-next-line property.notFound */
     $view = $this->widget->render();
-<<<<<<< HEAD
 
     expect($view)->toBeInstanceOf(View::class);
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-    expect($view)->toBeInstanceOf(View::class);
-=======
-    expect($view)->toBeInstanceOf(\Illuminate\Contracts\View\View::class);
->>>>>>> a12f125f4a (.)
-=======
-
-    expect($view)->toBeInstanceOf(View::class);
->>>>>>> b93ef594b4 (.)
-=======
-    
-    expect($view)->toBeInstanceOf(\Illuminate\Contracts\View\View::class);
->>>>>>> origin/develop
->>>>>>> 1899c5f (.)
 });
