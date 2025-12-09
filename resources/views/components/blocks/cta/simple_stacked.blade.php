1cacf65fd01b0309939eb04103d59c598c84d3cf
marco76tv <marco.sottana@gmail.com>
2025-12-09 19:02:00 +0100

    .

--- resources/views/components/blocks/cta/simple_stacked.blade.php
+++ resources/views/components/blocks/cta/simple_stacked.blade.php
@@ -1,8 +1,11 @@
+<<<<<<< HEAD
 <?php
 
 declare(strict_types=1);
 
 ?>
+=======
+>>>>>>> 727968c (.)
 <div class="bg-white">
     <div class="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:px-8">
       <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Boost your productivity.<br>Start using our app today.</h2>
@@ -12,3 +15,7 @@ declare(strict_types=1);
       </div>
     </div>
   </div>
+<<<<<<< HEAD
+=======
+  
+>>>>>>> 727968c (.)
--- resources/views/components/headernav/simple.blade.php
+++ resources/views/components/headernav/simple.blade.php
@@ -1,8 +1,11 @@
+<<<<<<< HEAD
 <?php
 
 declare(strict_types=1);
 
 ?>
+=======
+>>>>>>> 727968c (.)
 <header class="w-full">
     <div class="relative z-20 flex items-center justify-between w-full h-12 px-6 mx-auto">
         <div x-data="{ mobileMenuOpen: false }" class="relative flex items-center md:space-x-2 text-neutral-800">
