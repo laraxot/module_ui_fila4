76e2c09bd01d14bc48322d94ce0bc9d6d8efda1a
marco76tv <marco.sottana@gmail.com>
2025-12-09 18:50:07 +0100

    .

--- resources/views/components/blocks/cta/v1.blade.php
+++ resources/views/components/blocks/cta/v1.blade.php
@@ -1,26 +1,8 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> b93ef594b4 (.)
->>>>>>> 1899c5f (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
->>>>>>> a12f125f4a (.)
-=======
->>>>>>> b93ef594b4 (.)
-=======
->>>>>>> origin/develop
->>>>>>> 1899c5f (.)
 @props(['title', 'description', 'button_text', 'button_link'])
 
 <section class="bg-white">
@@ -43,20 +25,4 @@ declare(strict_types=1);
             </div>
         </div>
     </div>
-<<<<<<< HEAD
 </section>
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-</section>
-=======
-</section> 
->>>>>>> a12f125f4a (.)
-=======
-</section>
->>>>>>> b93ef594b4 (.)
-=======
-</section> 
->>>>>>> origin/develop
->>>>>>> 1899c5f (.)
