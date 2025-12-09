0e12e8adf43e69d2ab5f5abe2918cebb9dda227a
marco76tv <marco.sottana@gmail.com>
2025-12-09 18:42:44 +0100

    .

--- tests/Feature/InlineDatePickerTest.php
+++ tests/Feature/InlineDatePickerTest.php
@@ -9,11 +9,7 @@ use Carbon\Exceptions\InvalidFormatException;
 use Carbon\Carbon;
 use Filament\Forms\Components\Field;
 use Illuminate\Support\Facades\App;
-<<<<<<< HEAD
 use \Filament\Forms\Forms\Components\InlineDatePicker;
-=======
-use Modules\UI\Filament\Forms\Components\InlineDatePicker;
->>>>>>> 9c6c1e8 (.)
 
 uses(TestCase::class);
 
