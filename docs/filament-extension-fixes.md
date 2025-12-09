7b1524c147307d2110c8418ae84f63a0a2a627fc
marco76tv <marco.sottana@gmail.com>
2025-12-09 18:59:30 +0100

    .

--- docs/filament-extension-fixes.md
+++ docs/filament-extension-fixes.md
@@ -80,11 +80,6 @@ class S3Test extends XotBasePage
 
 ## Collegamenti
 - [Regole Estensione Filament](../../../.cursor/rules/filament-extension-rules.mdc)
-<<<<<<< HEAD
 - [XotBasePage Implementation](../../Xot/docs/xotbasepage_implementation.md)
 - [Filament Best Practices](../../../docs/FILAMENT-BEST-PRACTICES.md)
-=======
-- [XotBasePage Implementation](../../Xot/project_docs/xotbasepage_implementation.md)
-- [Filament Best Practices](../../../project_docs/FILAMENT-BEST-PRACTICES.md)
->>>>>>> 1899c5f (.)
 
--- resources/views/filament/forms/components/icon-picker.blade.php
+++ resources/views/filament/forms/components/icon-picker.blade.php
@@ -1,26 +1,8 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> b93ef594b4 (.)
->>>>>>> 1899c5f (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
->>>>>>> a12f125f4a (.)
-=======
->>>>>>> b93ef594b4 (.)
-=======
->>>>>>> origin/develop
->>>>>>> 1899c5f (.)
 {{--
 <x-filament-forms::field-wrapper :id="$getId()" :label="$getLabel()" :label-sr-only="$isLabelHidden()" :helper-text="$getHelperText()" :hint="$getHint()"
     :hint-icon="$getHintIcon()" :required="$isRequired()" :state-path="$getStatePath()">
@@ -44,17 +26,3 @@ declare(strict_types=1);
     </div>
 </x-filament-forms::field-wrapper>
 --}}
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
->>>>>>> a12f125f4a (.)
-=======
->>>>>>> b93ef594b4 (.)
-=======
-
->>>>>>> origin/develop
->>>>>>> 1899c5f (.)
