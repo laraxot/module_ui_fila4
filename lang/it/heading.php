94ec775224f647ae9514014a1aac7510034e1483
marco76tv <marco.sottana@gmail.com>
2025-12-09 18:53:05 +0100

    .

--- lang/it/heading.php
+++ lang/it/heading.php
@@ -1,5 +1,13 @@
 <?php
 
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> b93ef594b4 (.)
+>>>>>>> 1899c5f (.)
 declare(strict_types=1);
 
 return [
@@ -18,3 +26,35 @@ return [
         ],
     ],
 ];
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+=======
+=======
+>>>>>>> origin/develop
+return array (
+  'fields' => 
+  array (
+    'level' => 
+    array (
+      'label' => 'level',
+      'description' => 'level',
+      'helper_text' => 'level',
+      'placeholder' => 'level',
+    ),
+    'content' => 
+    array (
+      'label' => 'content',
+      'description' => 'content',
+      'helper_text' => 'content',
+      'placeholder' => 'content',
+    ),
+  ),
+);
+<<<<<<< HEAD
+>>>>>>> a12f125f4a (.)
+=======
+>>>>>>> b93ef594b4 (.)
+=======
+>>>>>>> origin/develop
+>>>>>>> 1899c5f (.)
