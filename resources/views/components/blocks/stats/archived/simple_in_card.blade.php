6da1820c16b2986dc3194c4ef24e1c63e535924d
marco76tv <marco.sottana@gmail.com>
2025-12-09 18:57:43 +0100

    .

--- resources/views/components/blocks/stats/archived/simple_in_card.blade.php
+++ resources/views/components/blocks/stats/archived/simple_in_card.blade.php
@@ -1,17 +1,8 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> ef3c5fa (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
-<<<<<<< HEAD
-=======
->>>>>>> 727968c (.)
-=======
->>>>>>> ef3c5fa (.)
 <div class="bg-gray-50 pt-12 sm:pt-16">
     <div class="mx-auto max-w-7xl px-6 lg:px-8">
       <div class="mx-auto max-w-4xl text-center">
@@ -43,10 +34,3 @@ declare(strict_types=1);
       </div>
     </div>
   </div>
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-  
->>>>>>> 727968c (.)
-=======
->>>>>>> ef3c5fa (.)
