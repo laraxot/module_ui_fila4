9e7951e78d95f72c4d960b824d628e8863ad0d02
marco76tv <marco.sottana@gmail.com>
2025-12-09 18:43:20 +0100

    .

--- app/Filament/Blocks/VideoSpatie.php
+++ app/Filament/Blocks/VideoSpatie.php
@@ -43,12 +43,12 @@ class VideoSpatie
                     ->previewable()
                     ->downloadable()
                     // ->rules(Rule::dimensions()->maxWidth(600)->maxHeight(800))
-                    ->collection(fn(\Filament\Schemas\Components\Utilities\Get $get) => $get('img_uuid'))
+                    ->collection(fn(Get $get) => $get('img_uuid'))
                     ->afterStateUpdated(function (
                         HasForms $_livewire,
                         SpatieMediaLibraryFileUpload $_component,
                         TemporaryUploadedFile $state,
-                        \Filament\Schemas\Components\Utilities\Get $get,
+                        Get $get,
                         HasMedia $record,
                     ) {
                         // Call to an undefined method Filament\Forms\Contracts\HasForms::validateOnly().
