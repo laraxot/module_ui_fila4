f79b1c4bcef74812a945860b7fdb8014e99dd2c7
marco76tv <marco.sottana@gmail.com>
2025-12-09 18:51:42 +0100

    .

--- resources/views/filament/widgets/empty.blade.php
+++ resources/views/filament/widgets/empty.blade.php
@@ -1,3 +1,11 @@
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> b93ef594b4 (.)
+>>>>>>> 1899c5f (.)
 <?php
 
 declare(strict_types=1);
@@ -6,3 +14,19 @@ declare(strict_types=1);
 <x-filament::widget >
     {{--  EMPTY  --}}
 </x-filament::widget >
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+=======
+<x-filament::widget >
+    {{--  EMPTY  --}}
+</x-filament::widget >    
+>>>>>>> a12f125f4a (.)
+=======
+>>>>>>> b93ef594b4 (.)
+=======
+<x-filament::widget >
+    {{--  EMPTY  --}}
+</x-filament::widget >    
+>>>>>>> origin/develop
+>>>>>>> 1899c5f (.)
