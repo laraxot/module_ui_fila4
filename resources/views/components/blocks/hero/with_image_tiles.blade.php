d663cf38a91d98872b88c82d740e82e05cfb4a22
marco76tv <marco.sottana@gmail.com>
2025-12-09 18:49:56 +0100

    .

--- resources/views/components/blocks/hero/with_image_tiles.blade.php
+++ resources/views/components/blocks/hero/with_image_tiles.blade.php
@@ -1,17 +1,8 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> ef3c5fa (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
-<<<<<<< HEAD
-=======
->>>>>>> 727968c (.)
-=======
->>>>>>> ef3c5fa (.)
 <div class="bg-white">
     <header class="absolute inset-x-0 top-0 z-50">
       <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
@@ -133,10 +124,3 @@ declare(strict_types=1);
       </div>
     </main>
   </div>
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-  
->>>>>>> 727968c (.)
-=======
->>>>>>> ef3c5fa (.)
