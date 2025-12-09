8b38325fbe82ee0bbb2ad2af3ccfddd4365f6cd1
marco76tv <marco.sottana@gmail.com>
2025-12-09 19:02:08 +0100

    .

--- resources/views/components/blocks/newsletter/side_by_side_on_card.blade.php
+++ resources/views/components/blocks/newsletter/side_by_side_on_card.blade.php
@@ -1,17 +1,8 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> ef3c5fa (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
-<<<<<<< HEAD
-=======
->>>>>>> 727968c (.)
-=======
->>>>>>> ef3c5fa (.)
 <!--
   This example requires some changes to your config:
 
@@ -50,10 +41,3 @@ declare(strict_types=1);
       </div>
     </div>
   </div>
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-  
->>>>>>> 727968c (.)
-=======
->>>>>>> ef3c5fa (.)
