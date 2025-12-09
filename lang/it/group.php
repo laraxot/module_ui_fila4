6365918a1dfc911b6faf0e78e9edf6a5196454a3
marco76tv <marco.sottana@gmail.com>
2025-12-09 18:46:33 +0100

    .

--- lang/it/group.php
+++ lang/it/group.php
@@ -1,6 +1,9 @@
 <?php
 
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> ef3c5fa (.)
 declare(strict_types=1);
 
 return [
@@ -31,6 +34,7 @@ return [
         ],
     ],
 ];
+<<<<<<< HEAD
 =======
 return array (
   'fields' => 
@@ -70,3 +74,5 @@ return array (
   ),
 );
 >>>>>>> 727968c (.)
+=======
+>>>>>>> ef3c5fa (.)
