f7250386de91188618d9c8c3e5b5b15bc0fc346f
marco76tv <marco.sottana@gmail.com>
2025-12-09 18:45:26 +0100

    .

--- lang/it/filament-navigation.php
+++ lang/it/filament-navigation.php
@@ -13,38 +13,10 @@ return [
         'created_at' => 'Data di creazione',
         'updated_at' => 'Data di aggiornamento',
     ],
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
->>>>>>> a12f125f4a (.)
-=======
->>>>>>> b93ef594b4 (.)
-=======
-
->>>>>>> origin/develop
->>>>>>> 1899c5f (.)
     'select-options' => [
         'same-tab' => 'Stessa scheda',
         'new-tab' => 'Nuova scheda',
     ],
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
->>>>>>> a12f125f4a (.)
-=======
->>>>>>> b93ef594b4 (.)
-=======
-
->>>>>>> origin/develop
->>>>>>> 1899c5f (.)
     'items' => [
         'empty' => 'Nessuna voce di menù.',
         'add-item' => 'Aggiungi voce di menù',
@@ -56,20 +28,6 @@ return [
         'remove' => 'Rimuovi',
         'sorted' => 'Ordinato',
     ],
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
->>>>>>> a12f125f4a (.)
-=======
->>>>>>> b93ef594b4 (.)
-=======
-
->>>>>>> origin/develop
->>>>>>> 1899c5f (.)
     'items-modal' => [
         'title' => 'Voce di menù',
         'label' => 'Etichetta',
