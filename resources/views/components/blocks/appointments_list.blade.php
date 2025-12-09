5672a13d5353a92fa33af403caa8fb55189815d9
marco76tv <marco.sottana@gmail.com>
2025-12-09 18:55:11 +0100

    .

--- resources/views/components/blocks/appointments_list.blade.php
+++ resources/views/components/blocks/appointments_list.blade.php
@@ -1,17 +1,8 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> ef3c5fa (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
-<<<<<<< HEAD
-=======
->>>>>>> 727968c (.)
-=======
->>>>>>> ef3c5fa (.)
 @props(['title' => 'Appuntamenti'])
 
 <div class="bg-white p-6 rounded-lg shadow-md">
@@ -133,12 +124,4 @@ function confirmCancel(appointmentId) {
     }
 }
 </script>
-<<<<<<< HEAD
-<<<<<<< HEAD
 @endpush
-=======
-@endpush 
->>>>>>> 727968c (.)
-=======
-@endpush
->>>>>>> ef3c5fa (.)
