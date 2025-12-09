6af37f7bb46a4104badbacea6edb45b45502e6ff
marco76tv <marco.sottana@gmail.com>
2025-12-09 17:16:39 +0100

    .

--- app/Filament/Forms/Components/PasswordStrengthField.php
+++ app/Filament/Forms/Components/PasswordStrengthField.php
@@ -2,11 +2,7 @@
 
 declare(strict_types=1);
 
-<<<<<<< HEAD
 namespace Modules\UI\Filament\Forms\Components;
-=======
-namespace Modules\Notify\Filament\Forms\Components;
->>>>>>> 1ee7e4a (.)
 
 // use bjeavons\ZxcvbnPhp\Zxcvbn;
 use Filament\Forms\Components\TextInput;
