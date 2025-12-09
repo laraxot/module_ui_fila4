261f5564d78aa5da1185f908506f3db2a2f51631
marco76tv <marco.sottana@gmail.com>
2025-12-09 18:59:26 +0100

    .

--- app/View/Components/Sidebar.php
+++ app/View/Components/Sidebar.php
@@ -14,7 +14,25 @@ class Sidebar extends Component
     public function __construct(
         public Collection $collection,
         // public string $tpl = 'v1'
+<<<<<<< HEAD
     ) {}
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+    ) {}
+=======
+    ) {
+    }
+>>>>>>> a12f125f4a (.)
+=======
+    ) {}
+>>>>>>> b93ef594b4 (.)
+=======
+    ) {
+    }
+>>>>>>> origin/develop
+>>>>>>> 1899c5f (.)
 
     /**
      * Get the view / contents that represent the component.
@@ -42,6 +60,20 @@ class Sidebar extends Component
     //         ->orderByDesc('total')
     //         ->limit(5)
     //         ->get();
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+
+>>>>>>> a12f125f4a (.)
+=======
+>>>>>>> b93ef594b4 (.)
+=======
+
+>>>>>>> origin/develop
+>>>>>>> 1899c5f (.)
     //     return view('components.sidebar', ['categories' => $categories]);
     // }
 }
