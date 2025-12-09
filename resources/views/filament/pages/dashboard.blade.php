657bc0a2b69dd33fd5299133175394aa052db00f
marco76tv <marco.sottana@gmail.com>
2025-12-09 19:07:40 +0100

    .

--- resources/views/filament/pages/dashboard.blade.php
+++ resources/views/filament/pages/dashboard.blade.php
@@ -1,17 +1,8 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> ef3c5fa (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
-<<<<<<< HEAD
-=======
->>>>>>> 727968c (.)
-=======
->>>>>>> ef3c5fa (.)
 <x-filament::page>
 
 </x-filament::page>
