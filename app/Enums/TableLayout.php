665a25012a5ebcffef802ddaf4ddf8c9ebb7ff3d
marco76tv <marco.sottana@gmail.com>
2025-12-09 18:56:42 +0100

    .

--- app/Enums/TableLayout.php
+++ app/Enums/TableLayout.php
@@ -11,7 +11,23 @@ enum TableLayout: string
 
     public function getLabel(): string
     {
+<<<<<<< HEAD
         return match ($this) {
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+        return match ($this) {
+=======
+        return match($this) {
+>>>>>>> a12f125f4a (.)
+=======
+        return match ($this) {
+>>>>>>> b93ef594b4 (.)
+=======
+        return match($this) {
+>>>>>>> origin/develop
+>>>>>>> 1899c5f (.)
             self::LIST => 'List View',
             self::GRID => 'Grid View',
         };
@@ -19,7 +35,23 @@ enum TableLayout: string
 
     public function getColor(): string
     {
+<<<<<<< HEAD
         return match ($this) {
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+        return match ($this) {
+=======
+        return match($this) {
+>>>>>>> a12f125f4a (.)
+=======
+        return match ($this) {
+>>>>>>> b93ef594b4 (.)
+=======
+        return match($this) {
+>>>>>>> origin/develop
+>>>>>>> 1899c5f (.)
             self::LIST => 'primary',
             self::GRID => 'secondary',
         };
@@ -27,7 +59,23 @@ enum TableLayout: string
 
     public function getIcon(): string
     {
+<<<<<<< HEAD
         return match ($this) {
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+        return match ($this) {
+=======
+        return match($this) {
+>>>>>>> a12f125f4a (.)
+=======
+        return match ($this) {
+>>>>>>> b93ef594b4 (.)
+=======
+        return match($this) {
+>>>>>>> origin/develop
+>>>>>>> 1899c5f (.)
             self::LIST => 'heroicon-o-list-bullet',
             self::GRID => 'heroicon-o-squares-2x2',
         };
@@ -35,7 +83,23 @@ enum TableLayout: string
 
     public function toggle(): self
     {
+<<<<<<< HEAD
         return match ($this) {
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+        return match ($this) {
+=======
+        return match($this) {
+>>>>>>> a12f125f4a (.)
+=======
+        return match ($this) {
+>>>>>>> b93ef594b4 (.)
+=======
+        return match($this) {
+>>>>>>> origin/develop
+>>>>>>> 1899c5f (.)
             self::LIST => self::GRID,
             self::GRID => self::LIST,
         };
