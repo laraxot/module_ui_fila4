d704926c9fc8c9694207ffc74ef112980179e542
marco76tv <marco.sottana@gmail.com>
2025-12-09 18:57:34 +0100

    .

--- app/Filament/Forms/Components/Field/QrReader.php
+++ app/Filament/Forms/Components/Field/QrReader.php
@@ -2,11 +2,7 @@
 
 declare(strict_types=1);
 
-<<<<<<< HEAD
-namespace \Filament\Forms\Forms\Components\Field;
-=======
-namespace Modules\Xot\Exceptions\Formatters;
->>>>>>> 19f3650 (.)
+namespace Modules\UI\Filament\Forms\Components\Field;
 
 use Filament\Forms\Components\Field;
 
