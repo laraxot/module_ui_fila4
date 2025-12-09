84da191424e1b1341276f520b505e516498f41b4
marco76tv <marco.sottana@gmail.com>
2025-12-09 19:05:09 +0100

    .

--- resources/views/livewire/dark-mode/switcher.blade.php
+++ resources/views/livewire/dark-mode/switcher.blade.php
@@ -1,11 +1,17 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> ef3c5fa (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
+<<<<<<< HEAD
 =======
 >>>>>>> 727968c (.)
+=======
+>>>>>>> ef3c5fa (.)
 <div>
     <button
         type="button"
@@ -47,7 +53,11 @@ declare(strict_types=1);
         });
     </script>
 <<<<<<< HEAD
+<<<<<<< HEAD
 </div>
 =======
 </div>
 >>>>>>> 727968c (.)
+=======
+</div>
+>>>>>>> ef3c5fa (.)
