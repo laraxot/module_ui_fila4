beaf74d4fd8dcbce484f0c573d4c432ee77357b8
marco76tv <marco.sottana@gmail.com>
2025-12-09 19:03:01 +0100

    .

--- resources/views/empty.blade.php
+++ resources/views/empty.blade.php
@@ -1,26 +1,6 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> b93ef594b4 (.)
->>>>>>> 1899c5f (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
 {{--  empty blade --}}
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-{{--  empty blade --}}
->>>>>>> a12f125f4a (.)
-=======
->>>>>>> b93ef594b4 (.)
-=======
-{{--  empty blade --}}
->>>>>>> origin/develop
->>>>>>> 1899c5f (.)
