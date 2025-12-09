9853b28e92f0d55e22f5e68c8cc98da1ddb48083
marco76tv <marco.sottana@gmail.com>
2025-12-09 18:45:02 +0100

    .

--- config/laravellocalization.php
+++ config/laravellocalization.php
@@ -1,5 +1,6 @@
 <?php
 
+<<<<<<< HEAD
 declare(strict_types=1);
 
 return [
@@ -11,17 +12,33 @@ return [
      * | This is the array for the languages
      * |
      */
+=======
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Languages
+    |--------------------------------------------------------------------------
+    |
+    | This is the array for the languages
+    |
+    */
+>>>>>>> 727968c (.)
     'languages' => [
         'it' => [
             'name' => 'Italiano',
             'script' => 'Latn',
             'native' => 'Italiano',
+<<<<<<< HEAD
             'regional' => 'it_IT',
+=======
+            'regional' => 'it_IT'
+>>>>>>> 727968c (.)
         ],
         'en' => [
             'name' => 'English',
             'script' => 'Latn',
             'native' => 'English',
+<<<<<<< HEAD
             'regional' => 'en_GB',
         ],
     ],
@@ -33,6 +50,20 @@ return [
      * | This is the configuration for the language detection
      * |
      */
+=======
+            'regional' => 'en_GB'
+        ]
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Language Detection
+    |--------------------------------------------------------------------------
+    |
+    | This is the configuration for the language detection
+    |
+    */
+>>>>>>> 727968c (.)
     'detectBrowserLanguage' => true,
     'detectBrowserLanguageFromAcceptLanguage' => true,
     'detectBrowserLanguageFromAcceptLanguageHeader' => true,
@@ -40,6 +71,7 @@ return [
     'detectBrowserLanguageFromCookie' => true,
     'detectBrowserLanguageFromQueryString' => true,
     'detectBrowserLanguageFromRoute' => true,
+<<<<<<< HEAD
     /*
      * |--------------------------------------------------------------------------
      * | Language Selection
@@ -48,12 +80,24 @@ return [
      * | This is the configuration for the language selection
      * |
      */
+=======
+
+    /*
+    |--------------------------------------------------------------------------
+    | Language Selection
+    |--------------------------------------------------------------------------
+    |
+    | This is the configuration for the language selection
+    |
+    */
+>>>>>>> 727968c (.)
     'hideDefaultLocaleInURL' => false,
     'useAcceptLanguageHeader' => true,
     'useSessionLocale' => true,
     'useCookieLocale' => true,
     'useQueryStringLocale' => true,
     'useRouteLocale' => true,
+<<<<<<< HEAD
     /*
      * |--------------------------------------------------------------------------
      * | Language Redirect
@@ -62,6 +106,17 @@ return [
      * | This is the configuration for the language redirect
      * |
      */
+=======
+
+    /*
+    |--------------------------------------------------------------------------
+    | Language Redirect
+    |--------------------------------------------------------------------------
+    |
+    | This is the configuration for the language redirect
+    |
+    */
+>>>>>>> 727968c (.)
     'redirectToDefaultLocale' => true,
     'redirectToDefaultLocaleIfNotSupported' => true,
     'redirectToDefaultLocaleIfNotInSupportedLocales' => true,
@@ -71,6 +126,7 @@ return [
     'redirectToDefaultLocaleIfNotInSupportedLocalesAndNotInCookie' => true,
     'redirectToDefaultLocaleIfNotInSupportedLocalesAndNotInQueryString' => true,
     'redirectToDefaultLocaleIfNotInSupportedLocalesAndNotInRoute' => true,
+<<<<<<< HEAD
     /*
      * |--------------------------------------------------------------------------
      * | Language Routes
@@ -79,6 +135,17 @@ return [
      * | This is the configuration for the language routes
      * |
      */
+=======
+
+    /*
+    |--------------------------------------------------------------------------
+    | Language Routes
+    |--------------------------------------------------------------------------
+    |
+    | This is the configuration for the language routes
+    |
+    */
+>>>>>>> 727968c (.)
     'useLocalizedRoutes' => true,
     'useLocalizedRoutesInMiddleware' => true,
     'useLocalizedRoutesInController' => true,
