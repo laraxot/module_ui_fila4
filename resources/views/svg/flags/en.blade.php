6752330b8c011e483deaf09c91d24e372bc81209
marco76tv <marco.sottana@gmail.com>
2025-12-09 18:55:16 +0100

    .

--- resources/views/svg/flags/en.blade.php
+++ resources/views/svg/flags/en.blade.php
@@ -1,10 +1,7 @@
 <?php
 
 declare(strict_types=1);
-<<<<<<< HEAD
-=======
 
->>>>>>> c6fab1a (.)
 ?>
 <svg xmlns="http://www.w3.org/2000/svg" class="size-5" id="flag-icons-us" viewBox="0 0 640 480">
     <path fill="#bd3d44" d="M0 0h640v480H0"/>
