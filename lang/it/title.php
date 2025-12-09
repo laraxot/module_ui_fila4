a10d6eaac300a0935034d9cdca4327b1ec698e60
marco76tv <marco.sottana@gmail.com>
2025-12-09 19:03:37 +0100

    .

--- lang/it/title.php
+++ lang/it/title.php
@@ -1,5 +1,6 @@
 <?php
 
+<<<<<<< HEAD
 declare(strict_types=1);
 
 return [
@@ -27,3 +28,35 @@ return [
         ],
     ],
 ];
+=======
+return array (
+  'fields' => 
+  array (
+    'text' => 
+    array (
+      'label' => 'Testo',
+      'description' => 'text',
+      'placeholder' => 'text',
+      'helper_text' => 'text',
+    ),
+    'level' => 
+    array (
+      'label' => 'Grandezza',
+      'description' => 'level',
+      'helper_text' => 'level',
+      'placeholder' => 'level',
+    ),
+    '_tpl' => 
+    array (
+      'label' => '_tpl',
+    ),
+    'view' => 
+    array (
+      'label' => 'view',
+      'description' => 'view',
+      'helper_text' => 'view',
+      'placeholder' => 'view',
+    ),
+  ),
+);
+>>>>>>> 727968c (.)
