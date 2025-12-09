5355b2974f7669114c40c7300b14ea2808af2991
marco76tv <marco.sottana@gmail.com>
2025-12-09 18:58:54 +0100

    .

--- app/Filament/Forms/Components/ParentSelect.php
+++ app/Filament/Forms/Components/ParentSelect.php
@@ -7,11 +7,8 @@ declare(strict_types=1);
  * @see https://github.com/ryangjchandler/filament-navigation
  */
 
-<<<<<<< HEAD
-namespace Modules\Notify\Filament\Forms\Components;
-=======
+
 namespace Modules\UI\Filament\Forms\Components;
->>>>>>> e3274ea (.)
 
 use Filament\Forms\Components\Select;
 
