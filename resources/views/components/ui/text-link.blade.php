0a321a956736884595a4a9e188421e002631f317
marco76tv <marco.sottana@gmail.com>
2025-12-09 19:02:46 +0100

    .

--- resources/views/components/ui/text-link.blade.php
+++ resources/views/components/ui/text-link.blade.php
@@ -1,27 +1,10 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> ef3c5fa (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
-<<<<<<< HEAD
-=======
->>>>>>> 727968c (.)
-=======
->>>>>>> ef3c5fa (.)
 <x-ui.link
     {{ $attributes->except('wire:navigate') }}
     class="text-gray-500 underline cursor-pointer dark:text-gray-400 dark:hover:text-gray-300 hover:text-gray-800">
     {{ $slot }}
-<<<<<<< HEAD
-<<<<<<< HEAD
 </x-ui.link>
-=======
-</x-ui.link>
->>>>>>> 727968c (.)
-=======
-</x-ui.link>
->>>>>>> ef3c5fa (.)
--- resources/views/livewire/dark-mode/switcher.blade.php
+++ resources/views/livewire/dark-mode/switcher.blade.php
@@ -1,17 +1,8 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> ef3c5fa (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
-<<<<<<< HEAD
-=======
->>>>>>> 727968c (.)
-=======
->>>>>>> ef3c5fa (.)
 <div>
     <button
         type="button"
@@ -52,12 +43,4 @@ declare(strict_types=1);
             }
         });
     </script>
-<<<<<<< HEAD
-<<<<<<< HEAD
 </div>
-=======
-</div>
->>>>>>> 727968c (.)
-=======
-</div>
->>>>>>> ef3c5fa (.)
