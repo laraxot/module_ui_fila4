0904925837b9f3c0d933dad71005aca20d99b0d0
marco76tv <marco.sottana@gmail.com>
2025-12-09 18:51:27 +0100

    .

--- app/Enums/TableLayout.php
+++ app/Enums/TableLayout.php
@@ -11,15 +11,7 @@ enum TableLayout: string
 
     public function getLabel(): string
     {
-<<<<<<< HEAD
-<<<<<<< HEAD
         return match ($this) {
-=======
-        return match($this) {
->>>>>>> 727968c (.)
-=======
-        return match ($this) {
->>>>>>> ef3c5fa (.)
             self::LIST => 'List View',
             self::GRID => 'Grid View',
         };
@@ -27,15 +19,7 @@ enum TableLayout: string
 
     public function getColor(): string
     {
-<<<<<<< HEAD
-<<<<<<< HEAD
         return match ($this) {
-=======
-        return match($this) {
->>>>>>> 727968c (.)
-=======
-        return match ($this) {
->>>>>>> ef3c5fa (.)
             self::LIST => 'primary',
             self::GRID => 'secondary',
         };
@@ -43,15 +27,7 @@ enum TableLayout: string
 
     public function getIcon(): string
     {
-<<<<<<< HEAD
-<<<<<<< HEAD
         return match ($this) {
-=======
-        return match($this) {
->>>>>>> 727968c (.)
-=======
-        return match ($this) {
->>>>>>> ef3c5fa (.)
             self::LIST => 'heroicon-o-list-bullet',
             self::GRID => 'heroicon-o-squares-2x2',
         };
@@ -59,15 +35,7 @@ enum TableLayout: string
 
     public function toggle(): self
     {
-<<<<<<< HEAD
-<<<<<<< HEAD
         return match ($this) {
-=======
-        return match($this) {
->>>>>>> 727968c (.)
-=======
-        return match ($this) {
->>>>>>> ef3c5fa (.)
             self::LIST => self::GRID,
             self::GRID => self::LIST,
         };
