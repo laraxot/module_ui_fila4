2e7512ddf1b3b460b46e10ff337af704cef15b67
marco76tv <marco.sottana@gmail.com>
2025-12-09 18:51:21 +0100

    .

--- resources/views/components/blocks/feature_sections/archivied/4x2_grid_on_brand.blade.php
+++ resources/views/components/blocks/feature_sections/archivied/4x2_grid_on_brand.blade.php
@@ -1,8 +1,11 @@
+<<<<<<< HEAD
 <?php
 
 declare(strict_types=1);
 
 ?>
+=======
+>>>>>>> 727968c (.)
 <div class="bg-indigo-700">
     <div class="mx-auto max-w-4xl px-6 py-24 sm:py-32 lg:max-w-7xl lg:px-8 lg:py-40">
       <h2 class="text-4xl font-bold tracking-tight text-white">Inbox support built for efficiency.</h2>
@@ -115,3 +118,7 @@ declare(strict_types=1);
       </div>
     </div>
   </div>
+<<<<<<< HEAD
+=======
+  
+>>>>>>> 727968c (.)
