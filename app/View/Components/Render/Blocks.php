8bb599796b627a1e15115957e89dc05496ba1681
marco76tv <marco.sottana@gmail.com>
2025-12-09 18:51:39 +0100

    .

--- app/View/Components/Render/Blocks.php
+++ app/View/Components/Render/Blocks.php
@@ -23,11 +23,33 @@ class Blocks extends Component
 {
     public array $blocks = [];
 
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> b93ef594b4 (.)
+>>>>>>> 1899c5f (.)
     public function __construct(
         ?array $blocks = [],
         public ?Model $model = null,
         public string $tpl = 'v1',
     ) {
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+=======
+    public function __construct(?array $blocks = [], public ?Model $model = null, public string $tpl = 'v1')
+    {
+>>>>>>> a12f125f4a (.)
+=======
+>>>>>>> b93ef594b4 (.)
+=======
+    public function __construct(?array $blocks = [], public ?Model $model = null, public string $tpl = 'v1')
+    {
+>>>>>>> origin/develop
+>>>>>>> 1899c5f (.)
         if (is_array($blocks)) {
             $this->blocks = $blocks;
         }
--- lang/it/send_email.php
+++ lang/it/send_email.php
@@ -1,5 +1,13 @@
 <?php
 
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> b93ef594b4 (.)
+>>>>>>> 1899c5f (.)
 declare(strict_types=1);
 
 return [
@@ -29,3 +37,49 @@ return [
         ],
     ],
 ];
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+=======
+=======
+>>>>>>> origin/develop
+return array (
+  'fields' => 
+  array (
+    'to' => 
+    array (
+      'label' => 'to',
+      'placeholder' => 'to',
+      'helper_text' => 'to',
+      'description' => 'to',
+    ),
+    'subject' => 
+    array (
+      'label' => 'subject',
+      'placeholder' => 'subject',
+      'helper_text' => 'subject',
+      'description' => 'subject',
+    ),
+    'body_html' => 
+    array (
+      'label' => 'body_html',
+      'placeholder' => 'body_html',
+      'helper_text' => 'body_html',
+      'description' => 'body_html',
+    ),
+  ),
+  'actions' => 
+  array (
+    'emailFormActions' => 
+    array (
+      'label' => 'emailFormActions',
+    ),
+  ),
+);
+<<<<<<< HEAD
+>>>>>>> a12f125f4a (.)
+=======
+>>>>>>> b93ef594b4 (.)
+=======
+>>>>>>> origin/develop
+>>>>>>> 1899c5f (.)
