8b7c9913e134345e2d96c21a91be83cd355f5db9
marco76tv <marco.sottana@gmail.com>
2025-12-09 18:53:37 +0100

    .

--- lang/nl/filament-navigation.php
+++ lang/nl/filament-navigation.php
@@ -13,24 +13,10 @@ return [
         'created_at' => 'Aangemaakt op',
         'updated_at' => 'Aangepast op',
     ],
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
->>>>>>> 727968c (.)
-=======
->>>>>>> ef3c5fa (.)
     'select-options' => [
         'same-tab' => 'Dezelfde tab',
         'new-tab' => 'Nieuwe tab',
     ],
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
->>>>>>> 727968c (.)
-=======
->>>>>>> ef3c5fa (.)
     'items' => [
         'empty' => 'Geen elementen.',
         'add-item' => 'Nieuw element',
@@ -41,13 +27,6 @@ return [
         'dedent' => 'Terugspringen',
         'remove' => 'Verwijderen',
     ],
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
->>>>>>> 727968c (.)
-=======
->>>>>>> ef3c5fa (.)
     'items-modal' => [
         'title' => 'Element',
         'label' => 'Label',
--- resources/views/components/ui/button.blade.php
+++ resources/views/components/ui/button.blade.php
@@ -1,17 +1,8 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> ef3c5fa (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
-<<<<<<< HEAD
-=======
->>>>>>> 727968c (.)
-=======
->>>>>>> ef3c5fa (.)
 @props([
     'type' => 'primary', 
     'size' => 'md', 
@@ -62,12 +53,4 @@ switch ($tag ?? 'button') {
 
 <{!! $tagAttr !!} {!! $attributes->except(['class']) !!} class="{{ $sizeClasses }} {{ $typeClasses }} cursor-pointer inline-flex items-center w-full justify-center disabled:opacity-50 font-semibold focus:outline-none">
     {{ $slot }}
-<<<<<<< HEAD
-<<<<<<< HEAD
 </{{ $tagClose }}>
-=======
-</{{ $tagClose }}>
->>>>>>> 727968c (.)
-=======
-</{{ $tagClose }}>
->>>>>>> ef3c5fa (.)
