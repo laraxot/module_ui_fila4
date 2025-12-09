d69a0a4d8bd8a2ff20356e06f7da19fecd3e7478
marco76tv <marco.sottana@gmail.com>
2025-12-09 18:45:05 +0100

    .

--- resources/views/components/blocks/notifications_list.blade.php
+++ resources/views/components/blocks/notifications_list.blade.php
@@ -1,11 +1,17 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> ef3c5fa (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
+<<<<<<< HEAD
 =======
 >>>>>>> 727968c (.)
+=======
+>>>>>>> ef3c5fa (.)
 @props(['title' => 'Notifiche'])
 
 <div class="bg-white p-6 rounded-lg shadow-md">
@@ -99,7 +105,11 @@ declare(strict_types=1);
         </div>
     @endif
 <<<<<<< HEAD
+<<<<<<< HEAD
 </div>
 =======
 </div> 
 >>>>>>> 727968c (.)
+=======
+</div>
+>>>>>>> ef3c5fa (.)
