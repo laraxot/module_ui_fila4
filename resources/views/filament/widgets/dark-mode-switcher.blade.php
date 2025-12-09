01e7703ffc8d3300f454aa95b6ed0e87e342d467
marco76tv <marco.sottana@gmail.com>
2025-12-09 18:43:54 +0100

    .

--- resources/views/filament/widgets/dark-mode-switcher.blade.php
+++ resources/views/filament/widgets/dark-mode-switcher.blade.php
@@ -1,8 +1,23 @@
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> b93ef594b4 (.)
+>>>>>>> 1899c5f (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+=======
+>>>>>>> a12f125f4a (.)
+=======
+>>>>>>> b93ef594b4 (.)
+>>>>>>> 1899c5f (.)
 <div class="dark-mode-switcher-widget">
     <button 
         x-data="{ darkMode: {{ $darkMode ? 'true' : 'false' }} }"
