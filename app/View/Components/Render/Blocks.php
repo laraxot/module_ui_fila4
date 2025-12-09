4b38aca155efa9645007054f1340579895564188
marco76tv <marco.sottana@gmail.com>
2025-12-09 18:49:43 +0100

    .

--- app/View/Components/Render/Blocks.php
+++ app/View/Components/Render/Blocks.php
@@ -24,15 +24,21 @@ class Blocks extends Component
     public array $blocks = [];
 
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> ef3c5fa (.)
     public function __construct(
         ?array $blocks = [],
         public ?Model $model = null,
         public string $tpl = 'v1',
     ) {
+<<<<<<< HEAD
 =======
     public function __construct(?array $blocks = [], public ?Model $model = null, public string $tpl = 'v1')
     {
 >>>>>>> 727968c (.)
+=======
+>>>>>>> ef3c5fa (.)
         if (is_array($blocks)) {
             $this->blocks = $blocks;
         }
