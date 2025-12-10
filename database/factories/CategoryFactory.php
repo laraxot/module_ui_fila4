a793c8136256ec7efa792c2feab0e400e1bede8e
marco76tv <marco.sottana@gmail.com>
2025-12-10 02:23:39 +0100

    .

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Modules\UI\Database\Factories;
 
+use Modules\UI\Models\Category;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Modules\UI\Models\Category;
 
--- database/factories/CollectionFactory.php
+++ database/factories/CollectionFactory.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Modules\UI\Database\Factories;
 
+use Modules\UI\Models\Collection;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Modules\UI\Models\Collection;
 
