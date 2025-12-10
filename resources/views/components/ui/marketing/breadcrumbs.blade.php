e2e1fe3c173e2e7b0679d497b2df17b9b0111dd5
marco76tv <marco.sottana@gmail.com>
2025-12-10 02:15:00 +0100

    .

--- resources/views/components/ui/marketing/breadcrumbs.blade.php
+++ resources/views/components/ui/marketing/breadcrumbs.blade.php
@@ -1,26 +1,8 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> b93ef594b4 (.)
->>>>>>> 1899c5f (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
->>>>>>> a12f125f4a (.)
-=======
->>>>>>> b93ef594b4 (.)
-=======
->>>>>>> origin/develop
->>>>>>> 1899c5f (.)
 @props([
     'crumbs' => [], 
     'page' => 'About'
@@ -53,20 +35,4 @@ Example Usage:
             @endif
         @endforeach
     </ol>
-<<<<<<< HEAD
 </nav>
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-</nav>
-=======
-</nav>
->>>>>>> a12f125f4a (.)
-=======
-</nav>
->>>>>>> b93ef594b4 (.)
-=======
-</nav>
->>>>>>> origin/develop
->>>>>>> 1899c5f (.)
