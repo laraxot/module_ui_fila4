d6f6188a5951ff5278a01d40adc7773de579962b
marco76tv <marco.sottana@gmail.com>
2025-12-10 02:12:31 +0100

    .

--- lang/en/datepicker.php
+++ lang/en/datepicker.php
@@ -4,14 +4,6 @@ declare(strict_types=1);
 
 return [
     'months' => [
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> b93ef594b4 (.)
->>>>>>> 1899c5f (.)
         'January',
         'February',
         'March',
@@ -24,10 +16,6 @@ return [
         'October',
         'November',
         'December',
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 1899c5f (.)
     ],
     'days_short' => [
         'mon' => 'Mon',
@@ -46,47 +34,6 @@ return [
         'friday' => 'Friday',
         'saturday' => 'Saturday',
         'sunday' => 'Sunday',
-<<<<<<< HEAD
-=======
-=======
-        'January', 'February', 'March', 'April', 'May', 'June',
-        'July', 'August', 'September', 'October', 'November', 'December',
-=======
->>>>>>> b93ef594b4 (.)
-    ],
-    'days_short' => [
-        'mon' => 'Mon',
-        'tue' => 'Tue',
-        'wed' => 'Wed',
-        'thu' => 'Thu',
-        'fri' => 'Fri',
-        'sat' => 'Sat',
-        'sun' => 'Sun',
-    ],
-    'days_full' => [
-<<<<<<< HEAD
-        'monday' => 'Monday', 'tuesday' => 'Tuesday', 'wednesday' => 'Wednesday', 'thursday' => 'Thursday', 'friday' => 'Friday', 'saturday' => 'Saturday', 'sunday' => 'Sunday',
->>>>>>> a12f125f4a (.)
-=======
-        'monday' => 'Monday',
-        'tuesday' => 'Tuesday',
-        'wednesday' => 'Wednesday',
-        'thursday' => 'Thursday',
-        'friday' => 'Friday',
-        'saturday' => 'Saturday',
-        'sunday' => 'Sunday',
->>>>>>> b93ef594b4 (.)
-=======
-        'January', 'February', 'March', 'April', 'May', 'June',
-        'July', 'August', 'September', 'October', 'November', 'December',
-    ],
-    'days_short' => [
-        'mon' => 'Mon', 'tue' => 'Tue', 'wed' => 'Wed', 'thu' => 'Thu', 'fri' => 'Fri', 'sat' => 'Sat', 'sun' => 'Sun',
-    ],
-    'days_full' => [
-        'monday' => 'Monday', 'tuesday' => 'Tuesday', 'wednesday' => 'Wednesday', 'thursday' => 'Thursday', 'friday' => 'Friday', 'saturday' => 'Saturday', 'sunday' => 'Sunday',
->>>>>>> origin/develop
->>>>>>> 1899c5f (.)
     ],
     'today' => 'Today',
     'select_date' => 'Select date',
--- resources/views/components/blocks/cta/simple_centered_on_brand.blade.php
+++ resources/views/components/blocks/cta/simple_centered_on_brand.blade.php
@@ -1,26 +1,8 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> b93ef594b4 (.)
->>>>>>> 1899c5f (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
->>>>>>> a12f125f4a (.)
-=======
->>>>>>> b93ef594b4 (.)
-=======
->>>>>>> origin/develop
->>>>>>> 1899c5f (.)
 <div class="bg-indigo-700">
     <div class="px-6 py-24 sm:px-6 sm:py-32 lg:px-8">
       <div class="mx-auto max-w-2xl text-center">
@@ -33,17 +15,3 @@ declare(strict_types=1);
       </div>
     </div>
   </div>
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-  
->>>>>>> a12f125f4a (.)
-=======
->>>>>>> b93ef594b4 (.)
-=======
-  
->>>>>>> origin/develop
->>>>>>> 1899c5f (.)
