db0994979fa393916b1db7b3f1c0b28d6e4f435c
marco76tv <marco.sottana@gmail.com>
2025-12-10 09:45:13 +0100

    .

--- resources/views/components/ui/modal.blade.php
+++ resources/views/components/ui/modal.blade.php
@@ -1,11 +1,17 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> ef3c5fa (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
+<<<<<<< HEAD
 =======
 >>>>>>> 727968c (.)
+=======
+>>>>>>> ef3c5fa (.)
 @props([
     'name',
     'show' => false,
