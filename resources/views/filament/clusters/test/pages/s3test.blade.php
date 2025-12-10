2440487c528b700568b2a6dce355e0fa0290518a
marco76tv <marco.sottana@gmail.com>
2025-12-10 02:00:40 +0100

    .

--- resources/views/filament/clusters/test/pages/s3test.blade.php
+++ resources/views/filament/clusters/test/pages/s3test.blade.php
@@ -15,7 +15,11 @@ declare(strict_types=1);
     ✅ Accessibile: Supporto screen reader
 --}}
 
+<<<<<<< HEAD
 <x-filament::page>
+=======
+<x-fi<x-filament::page>
+>>>>>>> 19f3650 (.)
     <x-filament::section>
         <x-slot name="heading">
             {{ __('ui::pages.s3test.heading') }}
