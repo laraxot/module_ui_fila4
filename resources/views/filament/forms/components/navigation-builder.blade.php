ac18768f28b09a448066cb10cf37749687af21b7
marco76tv <marco.sottana@gmail.com>
2025-12-10 02:19:32 +0100

    .

--- resources/views/filament/forms/components/navigation-builder.blade.php
+++ resources/views/filament/forms/components/navigation-builder.blade.php
@@ -1,8 +1,11 @@
+<<<<<<< HEAD
 <?php
 
 declare(strict_types=1);
 
 ?>
+=======
+>>>>>>> 727968c (.)
 @php
     $disableNewRecordCreation = $disableNewRecordCreation ?? false;
     $disableNewChildRecordCreation = $disableNewChildRecordCreation ?? false;
