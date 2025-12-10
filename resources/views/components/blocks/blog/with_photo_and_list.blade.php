baf13353116f59c410d0c73953c77f0bf5a194f6
marco76tv <marco.sottana@gmail.com>
2025-12-10 01:51:52 +0100

    .

--- resources/views/components/blocks/blog/with_photo_and_list.blade.php
+++ resources/views/components/blocks/blog/with_photo_and_list.blade.php
@@ -1,11 +1,17 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> ef3c5fa (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
+<<<<<<< HEAD
 =======
 >>>>>>> 727968c (.)
+=======
+>>>>>>> ef3c5fa (.)
 <div class="bg-white py-32">
     <div class="mx-auto max-w-7xl px-6 lg:px-8">
       <div class="mx-auto flex max-w-2xl flex-col items-end justify-between gap-16 lg:mx-0 lg:max-w-none lg:flex-row">
@@ -92,6 +98,9 @@ declare(strict_types=1);
     </div>
   </div>
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
   
 >>>>>>> 727968c (.)
+=======
+>>>>>>> ef3c5fa (.)
