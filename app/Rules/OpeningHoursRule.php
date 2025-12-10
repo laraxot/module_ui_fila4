17e471a7715a4f8e55e9cf961fcc8907816af74a
marco76tv <marco.sottana@gmail.com>
2025-12-10 02:23:23 +0100

    .

--- app/Rules/OpeningHoursRule.php
+++ app/Rules/OpeningHoursRule.php
@@ -9,7 +9,6 @@ namespace Modules\UI\Rules;
 use Illuminate\Contracts\Validation\ValidationRule;
 use Modules\UI\Actions\Datetime\GetDaysMappingAction;
 use Modules\Xot\Filament\Traits\TransTrait;
-
 use function Safe\preg_match;
 
 class OpeningHoursRule implements ValidationRule
