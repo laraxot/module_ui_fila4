79c2b9cef3c593274895e3a4a667f9be9bbbbb3d
marco76tv <marco.sottana@gmail.com>
2025-12-10 02:17:15 +0100

    .

--- resources/views/components/blocks/hero/split_with_navbar.blade.php
+++ resources/views/components/blocks/hero/split_with_navbar.blade.php
@@ -1,26 +1,8 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> b93ef594b4 (.)
->>>>>>> 1899c5f (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
->>>>>>> a12f125f4a (.)
-=======
->>>>>>> b93ef594b4 (.)
-=======
->>>>>>> origin/develop
->>>>>>> 1899c5f (.)
 <div class="relative bg-gray-50">
     <div class="relative bg-white shadow">
       <div class="mx-auto max-w-7xl px-6">
@@ -335,17 +317,3 @@ declare(strict_types=1);
       </div>
     </main>
   </div>
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-  
->>>>>>> a12f125f4a (.)
-=======
->>>>>>> b93ef594b4 (.)
-=======
-  
->>>>>>> origin/develop
->>>>>>> 1899c5f (.)
