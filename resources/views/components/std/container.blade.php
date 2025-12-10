3125404ba3494dccb57cf299ca8130f1eeaa9455
marco76tv <marco.sottana@gmail.com>
2025-12-10 02:13:51 +0100

    .

--- resources/views/components/std/container.blade.php
+++ resources/views/components/std/container.blade.php
@@ -1,8 +1,26 @@
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> b93ef594b4 (.)
+>>>>>>> 1899c5f (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+=======
+>>>>>>> a12f125f4a (.)
+=======
+>>>>>>> b93ef594b4 (.)
+=======
+>>>>>>> origin/develop
+>>>>>>> 1899c5f (.)
 <div {{ $attributes->merge(['class' => 'max-w-wide mx-auto p-2']) }}>
     {!! $slot !!}
 </div>
--- resources/views/filament/forms/components/radio-collection-item.blade.php
+++ resources/views/filament/forms/components/radio-collection-item.blade.php
@@ -1,8 +1,26 @@
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> b93ef594b4 (.)
+>>>>>>> 1899c5f (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+=======
+>>>>>>> a12f125f4a (.)
+=======
+>>>>>>> b93ef594b4 (.)
+=======
+>>>>>>> origin/develop
+>>>>>>> 1899c5f (.)
 {{-- resources/views/forms/components/radio-collection-item.blade.php --}}
 {{-- Template di esempio per un item - personalizzalo secondo le tue esigenze --}}
 <div class="flex items-center">
@@ -27,4 +45,20 @@ declare(strict_types=1);
             </div>
         @endif
     </div>
+<<<<<<< HEAD
 </div>
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+</div>
+=======
+</div>
+>>>>>>> a12f125f4a (.)
+=======
+</div>
+>>>>>>> b93ef594b4 (.)
+=======
+</div>
+>>>>>>> origin/develop
+>>>>>>> 1899c5f (.)
