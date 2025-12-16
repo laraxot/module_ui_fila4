0968b571dddec46b4000219fbbf1c47e07c68d26
marco76tv <marco.sottana@gmail.com>
2025-12-16 20:30:18 +0100

    .

--- resources/views/ui/studio-selector.blade.php
+++ resources/views/ui/studio-selector.blade.php
@@ -1,8 +1,26 @@
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> b93ef594b4 (.)
+>>>>>>> 1899c5f (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+=======
+>>>>>>> a12f125f4a (.)
+=======
+>>>>>>> b93ef594b4 (.)
+=======
+>>>>>>> origin/develop
+>>>>>>> 1899c5f (.)
 {{-- Studio Selector Component - Card cliccabili per selezione studio --}}
 <div 
     x-data="{
@@ -163,4 +181,20 @@ declare(strict_types=1);
             </p>
         </div>
     @endif
+<<<<<<< HEAD
 </div>
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+</div>
+=======
+</div> 
+>>>>>>> a12f125f4a (.)
+=======
+</div>
+>>>>>>> b93ef594b4 (.)
+=======
+</div> 
+>>>>>>> origin/develop
+>>>>>>> 1899c5f (.)
