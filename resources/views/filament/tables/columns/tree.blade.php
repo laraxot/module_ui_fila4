5108eac5604c267820ba90ce3f9e83f182805a20
marco76tv <marco.sottana@gmail.com>
2025-12-16 20:31:29 +0100

    .

--- resources/views/filament/tables/columns/tree.blade.php
+++ resources/views/filament/tables/columns/tree.blade.php
@@ -3,7 +3,11 @@
 declare(strict_types=1);
 
 ?>
+<<<<<<< HEAD
 <x-filament::field-wrapper
+=======
+<x-filament-forms::field-wrapper
+>>>>>>> c6fab1a (.)
 
     :label="$getLabel()"
 
@@ -33,4 +37,8 @@ declare(strict_types=1);
             {{ __('ui::filament-navigation.items.add-item') }}
         </x-filament::button>
     </div>
+<<<<<<< HEAD
 </x-filament::field-wrapper>
+=======
+</x-filament-forms::field-wrapper>
+>>>>>>> c6fab1a (.)
