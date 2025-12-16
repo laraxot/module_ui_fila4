2fb1b632ef18fcc700f98edfff39772d83773af8
marco76tv <marco.sottana@gmail.com>
2025-12-16 20:11:29 +0100

    .

--- resources/views/svg/flags/it.blade.php
+++ resources/views/svg/flags/it.blade.php
@@ -1,12 +1,46 @@
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> b93ef594b4 (.)
+>>>>>>> 1899c5f (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+=======
+>>>>>>> a12f125f4a (.)
+=======
+>>>>>>> b93ef594b4 (.)
+=======
+>>>>>>> origin/develop
+>>>>>>> 1899c5f (.)
 <svg xmlns="http://www.w3.org/2000/svg" class="size-5" id="flag-icons-it" viewBox="0 0 640 480">
     <g fill-rule="evenodd" stroke-width="1pt">
          <path fill="#fff" d="M0 0h640v480H0z"/>
          <path fill="#009246" d="M0 0h213.3v480H0z"/>
          <path fill="#ce2b37" d="M426.7 0H640v480H426.7z"/>
     </g>
+<<<<<<< HEAD
 </svg>
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+</svg>
+=======
+</svg>
+>>>>>>> a12f125f4a (.)
+=======
+</svg>
+>>>>>>> b93ef594b4 (.)
+=======
+</svg>
+>>>>>>> origin/develop
+>>>>>>> 1899c5f (.)
