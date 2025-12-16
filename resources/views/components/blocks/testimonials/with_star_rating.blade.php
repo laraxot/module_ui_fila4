d061a345865e4dfee585710b8449e0041c5363d3
marco76tv <marco.sottana@gmail.com>
2025-12-16 20:31:22 +0100

    .

--- resources/views/components/blocks/testimonials/with_star_rating.blade.php
+++ resources/views/components/blocks/testimonials/with_star_rating.blade.php
@@ -1,17 +1,8 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> ef3c5fa (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
-<<<<<<< HEAD
-=======
->>>>>>> 727968c (.)
-=======
->>>>>>> ef3c5fa (.)
 <section class="bg-white px-6 py-24 sm:py-32 lg:px-8">
     <figure class="mx-auto max-w-2xl">
       <p class="sr-only">5 out of 5 stars</p>
@@ -44,10 +35,3 @@ declare(strict_types=1);
       </figcaption>
     </figure>
   </section>
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-  
->>>>>>> 727968c (.)
-=======
->>>>>>> ef3c5fa (.)
