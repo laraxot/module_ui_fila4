22fc16606a045b4e34187c63c99b729ada678842
marco76tv <marco.sottana@gmail.com>
2025-12-16 20:21:25 +0100

    .

--- resources/views/components/blocks/patient_dashboard.blade.php
+++ resources/views/components/blocks/patient_dashboard.blade.php
@@ -1,26 +1,8 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> b93ef594b4 (.)
->>>>>>> 1899c5f (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
->>>>>>> a12f125f4a (.)
-=======
->>>>>>> b93ef594b4 (.)
-=======
->>>>>>> origin/develop
->>>>>>> 1899c5f (.)
 @props(['title' => 'Area Personale'])
 
 <div class="bg-white p-6 rounded-lg shadow-md">
@@ -128,20 +110,4 @@ declare(strict_types=1);
             <span class="text-gray-700">Notifiche</span>
         </a>
     </div>
-<<<<<<< HEAD
 </div>
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-</div>
-=======
-</div> 
->>>>>>> a12f125f4a (.)
-=======
-</div>
->>>>>>> b93ef594b4 (.)
-=======
-</div> 
->>>>>>> origin/develop
->>>>>>> 1899c5f (.)
