bbe90505475701bcfc001c13b64bbd385d21f3f6
marco76tv <marco.sottana@gmail.com>
2025-12-16 20:25:14 +0100

    .

--- resources/views/components/blocks/paragraph/agid.blade.php
+++ resources/views/components/blocks/paragraph/agid.blade.php
@@ -1,26 +1,6 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> b93ef594b4 (.)
->>>>>>> 1899c5f (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
 agid blade paragraph
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-agid blade paragraph
->>>>>>> a12f125f4a (.)
-=======
->>>>>>> b93ef594b4 (.)
-=======
-agid blade paragraph
->>>>>>> origin/develop
->>>>>>> 1899c5f (.)
