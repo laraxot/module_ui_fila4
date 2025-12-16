5426ab8fe897049777ca98cee54c14598e763e26
marco76tv <marco.sottana@gmail.com>
2025-12-16 20:26:03 +0100

    .

--- resources/views/filament/forms/components/icon-picker.blade.php
+++ resources/views/filament/forms/components/icon-picker.blade.php
@@ -4,11 +4,7 @@ declare(strict_types=1);
 
 ?>
 {{--
-<<<<<<< HEAD
-<x-filament::field-wrapper :id="$getId()" :label="$getLabel()" :label-sr-only="$isLabelHidden()" :helper-text="$getHelperText()" :hint="$getHint()"
-=======
 <x-filament-forms::field-wrapper :id="$getId()" :label="$getLabel()" :label-sr-only="$isLabelHidden()" :helper-text="$getHelperText()" :hint="$getHint()"
->>>>>>> c6fab1a (.)
     :hint-icon="$getHintIcon()" :required="$isRequired()" :state-path="$getStatePath()">
     <div x-data="{ state: $wire.{{ $applyStateBindingModifiers('entangle(\'' . $getStatePath() . '\')') }} }" class="flex items-center space-x-4">
 
@@ -28,9 +24,5 @@ declare(strict_types=1);
             </button>
 
     </div>
-<<<<<<< HEAD
-</x-filament::field-wrapper>
-=======
 </x-filament-forms::field-wrapper>
->>>>>>> c6fab1a (.)
 --}}
