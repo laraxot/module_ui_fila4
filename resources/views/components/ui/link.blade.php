19fcfdeab99488f5ffc5d1ecd8aefec0f3e09e55
marco76tv <marco.sottana@gmail.com>
2025-12-16 20:30:21 +0100

    .

--- resources/views/components/ui/link.blade.php
+++ resources/views/components/ui/link.blade.php
@@ -1,45 +1,11 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> b93ef594b4 (.)
->>>>>>> 1899c5f (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
->>>>>>> a12f125f4a (.)
-=======
->>>>>>> b93ef594b4 (.)
-=======
->>>>>>> origin/develop
->>>>>>> 1899c5f (.)
 <a
     {{ $attributes->except('wire:navigate') }}
     wire:navigate
 >
 {{ $slot }}
-<<<<<<< HEAD
 </a>
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-</a>
-=======
-</a>
->>>>>>> a12f125f4a (.)
-=======
-</a>
->>>>>>> b93ef594b4 (.)
-=======
-</a>
->>>>>>> origin/develop
->>>>>>> 1899c5f (.)
