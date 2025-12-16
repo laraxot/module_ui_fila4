9dc2905ca0d745f9e9d82db4693b7cd3a664061a
marco76tv <marco.sottana@gmail.com>
2025-12-16 20:27:40 +0100

    .

--- lang/it/slider.php
+++ lang/it/slider.php
@@ -1,5 +1,13 @@
 <?php
 
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> b93ef594b4 (.)
+>>>>>>> 1899c5f (.)
 declare(strict_types=1);
 
 return [
@@ -18,3 +26,36 @@ return [
         ],
     ],
 ];
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+=======
+=======
+>>>>>>> origin/develop
+return array (
+  'fields' => 
+  array (
+    'method' => 
+    array (
+      'label' => 'method',
+      'description' => 'method',
+      'helper_text' => 'method',
+      'placeholder' => 'method',
+    ),
+    '_tpl' => 
+    array (
+      'label' => '_tpl',
+    ),
+    'view' => 
+    array (
+      'label' => 'view',
+    ),
+  ),
+);
+<<<<<<< HEAD
+>>>>>>> a12f125f4a (.)
+=======
+>>>>>>> b93ef594b4 (.)
+=======
+>>>>>>> origin/develop
+>>>>>>> 1899c5f (.)
