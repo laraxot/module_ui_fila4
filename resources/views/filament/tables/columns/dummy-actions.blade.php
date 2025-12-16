a3c0b4dddfdfdefae6df4192dfadaee03e02f038
marco76tv <marco.sottana@gmail.com>
2025-12-16 20:15:08 +0100

    .

--- resources/views/filament/tables/columns/dummy-actions.blade.php
+++ resources/views/filament/tables/columns/dummy-actions.blade.php
@@ -1,29 +1,12 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> ef3c5fa (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
-<<<<<<< HEAD
-=======
->>>>>>> 727968c (.)
-=======
->>>>>>> ef3c5fa (.)
 <div class="flex gap-2 justify-center">
     @foreach($getActions() as $action)
         {!! $action->record($getRecord())->render() !!}
     @endforeach
 
     <x-filament-actions::modals />
-<<<<<<< HEAD
-<<<<<<< HEAD
 </div>
-=======
-</div>
->>>>>>> 727968c (.)
-=======
-</div>
->>>>>>> ef3c5fa (.)
