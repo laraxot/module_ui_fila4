df9b1f1f40678a168e9708ab258c4e3abf4897b8
marco76tv <marco.sottana@gmail.com>
2025-12-16 20:13:21 +0100

    .

--- app/Filament/Widgets/OverlookWidget.php
+++ app/Filament/Widgets/OverlookWidget.php
@@ -17,14 +17,6 @@ final class OverlookWidget extends XotBaseWidget
     public string $title = '';
 
     /*
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> b93ef594b4 (.)
->>>>>>> 1899c5f (.)
      * public array $grid = [
      * 'default' => 6,
      * 'sm' => 6,
@@ -34,39 +26,10 @@ final class OverlookWidget extends XotBaseWidget
      * '2xl' => null,
      * ];
      */
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-=======
->>>>>>> origin/develop
-        public array $grid = [
-                    'default' => 6,
-                    'sm' => 6,
-                    'md' => 6,
-                    'lg' => 6,
-                    'xl' => 6,
-                    '2xl' => null,
-                ];
-                */
-<<<<<<< HEAD
->>>>>>> a12f125f4a (.)
-=======
->>>>>>> b93ef594b4 (.)
->>>>>>> 1899c5f (.)
 
     public array $stats = [];
 
     protected string $view = 'ui::filament.widgets.overlook';
-<<<<<<< HEAD
-=======
-=======
-
-    public array $stats = [];
-
-    protected static string $view = 'ui::filament.widgets.overlook';
->>>>>>> origin/develop
->>>>>>> 1899c5f (.)
 
     protected int|string|array $columnSpan = 1;
 
@@ -76,14 +39,6 @@ final class OverlookWidget extends XotBaseWidget
     }
 
     /*
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> b93ef594b4 (.)
->>>>>>> 1899c5f (.)
      * public function mount(array $filter): void
      * {
      * $this->filter = $filter;
@@ -102,35 +57,4 @@ final class OverlookWidget extends XotBaseWidget
      * }
      * }
      */
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-=======
->>>>>>> origin/develop
-    public function mount(array $filter): void
-    {
-        $this->filter = $filter;
-
-        $this->data = $this->getData();
-        // dddx($this->data);
-        if (empty($this->grid)) {
-            $this->grid = [
-                'default' => 2,
-                'sm' => 2,
-                'md' => 3,
-                'lg' => 3,
-                'xl' => 3,
-                '2xl' => null,
-            ];
-        }
-    }
-    */
-<<<<<<< HEAD
->>>>>>> a12f125f4a (.)
-=======
->>>>>>> b93ef594b4 (.)
-=======
->>>>>>> origin/develop
->>>>>>> 1899c5f (.)
 }
