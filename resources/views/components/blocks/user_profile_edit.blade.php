93e1af19171fba40963c1dc232c01fd096cb93ee
marco76tv <marco.sottana@gmail.com>
2025-12-16 20:33:01 +0100

    .

--- resources/views/components/blocks/user_profile_edit.blade.php
+++ resources/views/components/blocks/user_profile_edit.blade.php
@@ -1,8 +1,26 @@
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> b93ef594b4 (.)
+>>>>>>> 1899c5f (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+=======
+>>>>>>> a12f125f4a (.)
+=======
+>>>>>>> b93ef594b4 (.)
+=======
+>>>>>>> origin/develop
+>>>>>>> 1899c5f (.)
 @props(['title' => 'Modifica Profilo'])
 
 <div class="bg-white p-6 rounded-lg shadow-md">
