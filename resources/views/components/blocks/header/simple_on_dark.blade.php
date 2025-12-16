1f2bb24f7ddf272473f6f6a2817fc2fc5c92bb6f
marco76tv <marco.sottana@gmail.com>
2025-12-16 20:24:56 +0100

    .

--- resources/views/components/blocks/header/simple_on_dark.blade.php
+++ resources/views/components/blocks/header/simple_on_dark.blade.php
@@ -1,17 +1,8 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> ef3c5fa (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
-<<<<<<< HEAD
-=======
->>>>>>> 727968c (.)
-=======
->>>>>>> ef3c5fa (.)
 <div class="bg-gray-900 py-24 sm:py-32">
     <div class="mx-auto max-w-7xl px-6 lg:px-8">
       <div class="mx-auto max-w-2xl lg:mx-0">
@@ -20,10 +11,3 @@ declare(strict_types=1);
       </div>
     </div>
   </div>
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-  
->>>>>>> 727968c (.)
-=======
->>>>>>> ef3c5fa (.)
--- resources/views/components/blocks/stats/v1.blade.php
+++ resources/views/components/blocks/stats/v1.blade.php
@@ -1,17 +1,8 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> ef3c5fa (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
-<<<<<<< HEAD
-=======
->>>>>>> 727968c (.)
-=======
->>>>>>> ef3c5fa (.)
 @props(['title', 'stats'])
 
 <section class="bg-indigo-700">
@@ -34,12 +25,4 @@ declare(strict_types=1);
             @endforeach
         </dl>
     </div>
-<<<<<<< HEAD
-<<<<<<< HEAD
 </section>
-=======
-</section> 
->>>>>>> 727968c (.)
-=======
-</section>
->>>>>>> ef3c5fa (.)
