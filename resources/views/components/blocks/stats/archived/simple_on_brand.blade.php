5d8fd4634edc4ca459fc67c8f518f3eedecedc69
marco76tv <marco.sottana@gmail.com>
2025-12-16 20:17:49 +0100

    .

--- resources/views/components/blocks/stats/archived/simple_on_brand.blade.php
+++ resources/views/components/blocks/stats/archived/simple_on_brand.blade.php
@@ -1,17 +1,8 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> ef3c5fa (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
-<<<<<<< HEAD
-=======
->>>>>>> 727968c (.)
-=======
->>>>>>> ef3c5fa (.)
 <div class="bg-indigo-800">
     <div class="mx-auto max-w-7xl px-6 py-12 sm:py-16 lg:px-8 lg:py-20">
       <div class="mx-auto max-w-4xl text-center">
@@ -34,10 +25,3 @@ declare(strict_types=1);
       </dl>
     </div>
   </div>
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-  
->>>>>>> 727968c (.)
-=======
->>>>>>> ef3c5fa (.)
