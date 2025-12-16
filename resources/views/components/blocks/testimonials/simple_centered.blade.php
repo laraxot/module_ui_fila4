e38be22fea8ee3c9e5cacba5a2ecd3b8713eb68d
marco76tv <marco.sottana@gmail.com>
2025-12-16 20:21:01 +0100

    .

--- resources/views/components/blocks/testimonials/simple_centered.blade.php
+++ resources/views/components/blocks/testimonials/simple_centered.blade.php
@@ -1,8 +1,11 @@
+<<<<<<< HEAD
 <?php
 
 declare(strict_types=1);
 
 ?>
+=======
+>>>>>>> 727968c (.)
 <section class="relative isolate overflow-hidden bg-white px-6 py-24 sm:py-32 lg:px-8">
     <div class="absolute inset-0 -z-10 bg-[radial-gradient(45rem_50rem_at_top,theme(colors.indigo.100),white)] opacity-20"></div>
     <div class="absolute inset-y-0 right-1/2 -z-10 mr-16 w-[200%] origin-bottom-left skew-x-[-30deg] bg-white shadow-xl shadow-indigo-600/10 ring-1 ring-indigo-50 sm:mr-28 lg:mr-0 xl:mr-16 xl:origin-center"></div>
@@ -25,3 +28,7 @@ declare(strict_types=1);
       </figure>
     </div>
   </section>
+<<<<<<< HEAD
+=======
+  
+>>>>>>> 727968c (.)
--- resources/views/components/layouts/marketing.blade.php
+++ resources/views/components/layouts/marketing.blade.php
@@ -1,12 +1,19 @@
+<<<<<<< HEAD
 <?php
 
 declare(strict_types=1);
 
 ?>
+=======
+>>>>>>> 727968c (.)
 <x-layouts.main>
     
     <x-ui.marketing.header />
     
     {{ $slot }}
 
+<<<<<<< HEAD
 </x-layouts.main>
+=======
+</x-layouts.main>
+>>>>>>> 727968c (.)
