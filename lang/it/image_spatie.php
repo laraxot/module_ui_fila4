de4aae1f53ddcc76475adeae954ff37b291322b2
marco76tv <marco.sottana@gmail.com>
2025-12-16 20:28:40 +0100

    .

--- lang/it/image_spatie.php
+++ lang/it/image_spatie.php
@@ -1,5 +1,6 @@
 <?php
 
+<<<<<<< HEAD
 declare(strict_types=1);
 
 return [
@@ -24,3 +25,31 @@ return [
         ],
     ],
 ];
+=======
+return array (
+  'fields' => 
+  array (
+    'caption' => 
+    array (
+      'label' => 'caption',
+      'description' => 'caption',
+      'helper_text' => 'caption',
+      'placeholder' => 'caption',
+    ),
+    'image' => 
+    array (
+      'label' => 'image',
+      'description' => 'image',
+      'helper_text' => 'image',
+      'placeholder' => 'image',
+    ),
+    'img_uuid' => 
+    array (
+      'label' => 'img_uuid',
+      'description' => 'img_uuid',
+      'helper_text' => 'img_uuid',
+      'placeholder' => 'img_uuid',
+    ),
+  ),
+);
+>>>>>>> 727968c (.)
