522769f5fd299af9dbdeaf397de888cf790df02d
marco76tv <marco.sottana@gmail.com>
2026-01-05 22:49:45 +0100

    .

--- app/Data/UserData.php
+++ app/Data/UserData.php
@@ -6,15 +6,16 @@ namespace Modules\UI\Data;
 
 use Spatie\LaravelData\Data;
 
-class UserData extends Data
+final class UserData extends Data
 {
     public function __construct(
         public int $id,
         public string $name,
         public string $email,
-        public null|string $avatar,
-        public null|string $role,
+        public ?string $avatar,
+        public ?string $role,
         public array $permissions,
         public array $settings,
-    ) {}
+    ) {
+    }
 }
--- app/Filament/Forms/Components/ParentSelect.php
+++ app/Filament/Forms/Components/ParentSelect.php
@@ -2,31 +2,8 @@
 
 declare(strict_types=1);
 
-/**
- * @see RyanChandler\FilamentNavigation\Filament\Fields\NavigationSelect;
- * @see https://github.com/ryangjchandler/filament-navigation
- */
-
 namespace Modules\UI\Filament\Forms\Components;
 
-use Filament\Forms\Components\Select;
+use Modules\Xot\Filament\Forms\Components\XotBaseSelect;
 
-// use RyanChandler\FilamentNavigation\Models\Navigation;
-
-class ParentSelect extends Select
-{
-    protected string $optionValueProperty = 'id';
-
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        // dddx($this->getModel());
-        $this->options(static fn(ParentSelect $_component): array => ['a' => 'a', 'b' => 'b']);
-    }
-
-    public function getOptionValueProperty(): string
-    {
-        return $this->optionValueProperty;
-    }
-}
+class ParentSelect extends XotBaseSelect {}
