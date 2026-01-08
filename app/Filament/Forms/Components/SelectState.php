cfd04687a6b61d6d31c213792f926d267f88fb98
marco76tv <marco.sottana@gmail.com>
2026-01-08 14:33:41 +0100

    .

--- app/Filament/Forms/Components/SelectState.php
+++ app/Filament/Forms/Components/SelectState.php
@@ -2,11 +2,7 @@
 
 declare(strict_types=1);
 
-<<<<<<< HEAD
 namespace Modules\Notify\Filament\Forms\Components;
-=======
-namespace Modules\UI\Filament\Forms\Components;
->>>>>>> e3274ea (.)
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
