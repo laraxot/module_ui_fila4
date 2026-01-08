003f225889ab771fd06f8692b5ee97e081747a53
marco76tv <marco.sottana@gmail.com>
2026-01-08 14:39:38 +0100

    .

--- app/Filament/Forms/Components/Children.php
+++ app/Filament/Forms/Components/Children.php
@@ -17,14 +17,6 @@ final class Children extends XotBaseViewField
     protected string $view = 'ui::filament.forms.components.navigation-builder';
 
     /*
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> b93ef594b4 (.)
->>>>>>> 1899c5f (.)
      * public static function make($livewire): static
      * {
      * $result = app(static::class, ['livewire' => $livewire]);
@@ -32,24 +24,4 @@ final class Children extends XotBaseViewField
      * return $result;
      * }
      */
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-=======
->>>>>>> origin/develop
-    public static function make($livewire): static
-    {
-        $result = app(static::class, ['livewire' => $livewire]);
-        $result->configure();
-        return $result;
-    }
-    */
-<<<<<<< HEAD
->>>>>>> a12f125f4a (.)
-=======
->>>>>>> b93ef594b4 (.)
-=======
->>>>>>> origin/develop
->>>>>>> 1899c5f (.)
 }
--- app/Filament/Forms/Components/Field/QrReader.php
+++ app/Filament/Forms/Components/Field/QrReader.php
@@ -11,14 +11,6 @@ final class QrReader extends XotBaseField
     protected string $view = 'ui::filament.forms.components.field.qr-reader';
 
     /*
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> b93ef594b4 (.)
->>>>>>> 1899c5f (.)
      * public static function make($livewire): static
      * {
      * $result = app(static::class, ['livewire' => $livewire]);
@@ -26,24 +18,4 @@ final class QrReader extends XotBaseField
      * return $result;
      * }
      */
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-=======
->>>>>>> origin/develop
-    public static function make($livewire): static
-    {
-        $result = app(static::class, ['livewire' => $livewire]);
-        $result->configure();
-        return $result;
-    }
-    */
-<<<<<<< HEAD
->>>>>>> a12f125f4a (.)
-=======
->>>>>>> b93ef594b4 (.)
-=======
->>>>>>> origin/develop
->>>>>>> 1899c5f (.)
 }
