c5d75a694a3b07ecb6117a3aa96cc79f43dcfc25
marco76tv <marco.sottana@gmail.com>
2026-01-08 14:28:07 +0100

    .

--- app/Filament/Forms/Components/RadioImage.php
+++ app/Filament/Forms/Components/RadioImage.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Modules\Notify\Filament\Forms\Components;
+namespace Modules\UI\Filament\Forms\Components;
 
 use Modules\Xot\Filament\Forms\Components\XotBaseRadio;
 
