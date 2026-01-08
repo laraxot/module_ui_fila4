4879940f5ddd73dcb60ca889d7a8da2c04487af0
marco76tv <marco.sottana@gmail.com>
2026-01-08 14:32:43 +0100

    .

--- app/Filament/Widgets/RowWidget.php
+++ app/Filament/Widgets/RowWidget.php
@@ -17,15 +17,7 @@ abstract class RowWidget extends XotBaseWidget
 
     public array $widgets = [];
 
-<<<<<<< HEAD
     protected string $view = 'ui::filament.widgets.row';
-=======
-<<<<<<< HEAD
-    protected string $view = 'ui::filament.widgets.row';
-=======
-    protected static string $view = 'ui::filament.widgets.row';
->>>>>>> origin/develop
->>>>>>> 1899c5f (.)
 
     protected int|string|array $columnSpan = 'full';
 
