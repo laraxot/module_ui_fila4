2b5960d9c8ebeff17c92c59116a4352df8720fe2
marco76tv <marco.sottana@gmail.com>
2026-01-08 14:37:37 +0100

    .

--- app/Filament/Forms/Components/RadioCollection.php
+++ app/Filament/Forms/Components/RadioCollection.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Modules\Notify\Filament\Forms\Components;
+namespace Modules\UI\Filament\Forms\Components;
 
 use Illuminate\Support\Collection;
 use Modules\Xot\Actions\Cast\SafeStringCastAction;
